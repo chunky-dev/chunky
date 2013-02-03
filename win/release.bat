319c2fbf5a83d22e8727f5397b90b21b67f85af5
Jesper Öqvist <jesper@llbit.se>
2013-02-03 13:23:31 +0100

    Improved Windows release script

--- win/release.bat
+++ win/release.bat
@@ -1,7 +1,9 @@
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_31
 set ANT_HOME=C:\Program Files\apache-ant-1.8.2
-set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
+set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin;C:\Program Files (x86)\NSIS
 echo PATH=%PATH%
 cd ..
 call ant release
+cd win
+call makensis Chunky.nsi
 pause
