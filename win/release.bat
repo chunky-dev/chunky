c388bef3806eb84723470a2a6249861b82d8612c
Jesper Öqvist <jesper@llbit.se>
2013-02-03 13:09:45 +0100

    Fixed error in build script

--- win/release.bat
+++ win/release.bat
@@ -1,5 +1,7 @@
-@echo off
 set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_31
 set ANT_HOME=C:\Program Files\apache-ant-1.8.2
 set PATH=%JAVA_HOME%\bin;%ANT_HOME%\bin
 echo PATH=%PATH%
+cd ..
+call ant release
+pause
