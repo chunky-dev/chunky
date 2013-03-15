65dd25dbae2947b391097c96516292501845341d
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-03-15 16:09:26 +0100

    Updated test script

--- test.sh
+++ test.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-#JAVA_OPTS=-XX:+UnlockDiagnosticVMOptions \
-#    -XX:+PrintAssembly \
-#    -XX:CompileCommand=print,Octree.intersect
-#JAVA_OPTS=-XX:+PrintCompilation
-
-if [ ! -d test ]; then
-	mkdir test
+if [ $# -lt "1" ]; then
+    echo "Usage: $0 <BLOCK ID[:METADATA]>"
+    exit 1
 fi
 
-if ant clean && ant -Ddebug=true jar; then
-    (cd test; java $JAVA_OPTS -jar ../build/Chunky.jar)
+JAVA_OPTS="-Xmx12g -Xms512m"
+
+if ant -Ddebug=true build; then
+	if [ ! -d test ]; then
+		mkdir test
+	fi
+
+	cd test
+    java $JAVA_OPTS -cp ../bin:../lib/j99.jar:../lib/JOCL-0.1.7.jar:../lib/log4j-1.2.17.jar se.llbit.chunky.main.BlockTestRenderer $1
 fi
 
