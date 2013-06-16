586a6e13eaf6ee002b03786ac5ba338807aba2f4
Jesper Öqvist <jesper@llbit.se>
2013-06-16 11:54:31 +0200

    Added more benchmarking scripts

--- bench/benchmark.sh
+++ bench/benchmark.sh
@@ -6,22 +6,25 @@
 
 interrupted()
 {
-    exit $?
+	exit $?
 }
 
 trap interrupted SIGINT
 
 JAVA_OPTS="-Xmx4g -Xms512m"
 
-if [ ! -d test ]; then
-	mkdir test
+if ! (ant -f ../build.xml clean jar); then
+	echo "failed to build chunky!"
+	exit 1
 fi
 
-if ant clean && ant jar; then
+for ((i=1;i<=10;++i)); do
+	echo "benchmark run $i/10"
+	echo "writing output to log$$-1.dat"
 	for tilewidth in 1 2 4 8 16 32 64 128 256; do
 		echo -n "$tilewidth "
-    	(cd test; java $JAVA_OPTS -jar ../build/Chunky.jar -benchmark -threads 8 -tile-width $tilewidth) | grep 'Benchmark completed' | cut -d ' ' -f 5
-	done
-fi
-
+		java $JAVA_OPTS -jar ../build/Chunky.jar -benchmark -threads 8 -tile-width $tilewidth | \
+			grep 'Benchmark completed' | cut -d ' ' -f 5
+	done | tee log$$-$i.dat
+done
 
---
+++ bench/getresults.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import sys
+import math
+import os
+import re
+
+# 90% confidence interval with 9 (n-1) degrees of freedom
+t = [ 6.314, 2.920, 2.353, 2.132, 2.015, 1.943, 1.895, 1.860, 1.833, 1.812,
+1.796, 1.782, 1.771, 1.761, 1.753, 1.746, 1.740, 1.734, 1.729, 1.725,
+1.721, 1.717, 1.714, 1.711, 1.708, 1.706, 1.703, 1.701, 1.699, 1.697 ]
+
+# results: tilewidth -> spp measurements
+results = { }
+
+# calculate confidence intervals using student's t distribution
+def calcdist():
+	global results
+	for w in sorted(results):
+		spp = results[w]
+		sum = 0
+		n = len(spp)
+		for x in spp:
+			sum += x
+
+		mean = sum / n
+
+		p = 0
+		for x in spp:
+			p += math.pow(x - mean, 2)
+
+		# std deviation
+		s = math.sqrt(p / (n-1))
+
+		c1 = mean - t[n-1] * (s / math.sqrt(n))
+		c2 = mean + t[n-1] * (s / math.sqrt(n))
+		print w, mean, c1, c2
+
+def parselog(path):
+	global results
+	with open(path) as f:
+		for line in f.readlines():
+			items = line.split()
+			w = int(items[0])
+			spp = float(items[1])
+			if w not in results:
+				results[w] = []
+			results[w].append(spp)
+
+def main():
+	if len(sys.argv) < 2:
+		print 'usage: calcdist.py <LOGFILES>'
+		sys.exit(1)
+		return
+
+	for i in range(1, len(sys.argv)):
+		parselog(sys.argv[i])
+
+	calcdist()
+
+if __name__ == "__main__":
+	main()
