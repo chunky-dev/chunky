9b9e6b72f10b95b434b1fe288941ea4b74442f50
Jesper Öqvist <jesper@llbit.se>
2013-06-17 00:24:59 +0200

    Updated eclipse project
    - add commons-math jar to build path
    - add JUnit4 to build path
    - added two basic QuickMath tests

---
+++ src/test/java/se/llbit/math/TestQuickMath.java
@@ -0,0 +1,36 @@
+package se.llbit.math;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * TODO add more tests
+ * TODO test NaN parameters
+ * TODO test +-0.0 parameters
+ * @author Jesper Öqvist <jesper@llbit.se>
+ */
+public class TestQuickMath {
+
+	/**
+	 * Test double precision minimum func.
+	 */
+	@Test
+	public void testMin_1() {
+		assertTrue(11.0 == QuickMath.min(132.0, 11.0));
+		assertTrue(11.0 == QuickMath.min(11.0, 132.0));
+		assertTrue(-132.0 == QuickMath.min(-132.0, -11.0));
+		assertTrue(-132.0 == QuickMath.min(-11.0, -132.0));
+	}
+
+	/**
+	 * Test double precision minimum func.
+	 */
+	@Test
+	public void testAbs_1() {
+		assertTrue(1.0 == QuickMath.abs(1.0));
+		assertTrue(1.0 == QuickMath.abs(-1.0));
+		assertTrue(0.0 == QuickMath.abs(0.0));
+		assertTrue(0.0 == QuickMath.abs(-0.0));
+	}
+}
