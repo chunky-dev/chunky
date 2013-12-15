1c6b900e02a1a48a734a37f7731523f0444553d0
Jesper Öqvist <jesper@llbit.se>
2013-12-15 20:50:12 +0100

    Refactored biome data representation

--- chunky/src/java/se/llbit/chunky/world/Biomes.java
+++ chunky/src/java/se/llbit/chunky/world/Biomes.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012 Jesper Öqvist <jesper@llbit.se>
+/* Copyright (c) 2012-2013 Jesper Öqvist <jesper@llbit.se>
  *
  * This file is part of Chunky.
  *
@@ -30,95 +30,47 @@ import se.llbit.math.QuickMath;
  */
 public class Biomes {
 	private static final int SWAMP_ID = 6;
-	/**
-	 * Temperature and rain values, clamped to [0,1]
-	 */
-	private static final float[][] tempAndRain = {
-			{0.5f, 0.5f},// ocean
-			{0.8f, 0.4f},// plains
-			{1.0f, 0.0f},// desert
-			{0.2f, 0.3f},// extreme hills
-			{0.7f, 0.8f},// forest
-			{0.05f,0.8f},// taiga
-			{0.8f, 0.9f},// swampland
-			{0.5f, 0.5f},// river
-			{1.0f, 0.0f},// hell
-			{0.5f, 0.5f},// sky
-			{0.0f, 0.5f},// frozen ocean
-			{0.0f, 0.5f},// frozen river
-			{0.0f, 0.5f},// ice plains
-			{0.0f, 0.5f},// ice mountains
-			{0.9f, 1.0f},// mushroom island
-			{0.9f, 1.0f},// mushroom island shore
-			{0.8f, 0.4f},// beach
-			{1.0f, 0.0f},// desert hills
-			{0.7f, 0.8f},// forest hills
-			{0.05f,0.8f},// taiga hills
-			{0.2f, 0.3f},// extreme hills edge
-			{1.0f, 0.9f},// jungle
-			{1.0f, 0.9f},// jungle hills
-			{0.95f, 0.8f},// jungle edge
-			{0.5f, 0.5f},// deep ocean
-			{0.2f, 0.3f},// stone beach
-			{0.05f, 0.3f},// cold beach
-			{0.7f, 0.8f},// birch forest
-			{0.7f, 0.8f},// birch forest hills
-			{0.7f, 0.8f},// roofed forest
-			{-0.5f, 0.4f},// cold taiga
-			{-0.5f, 0.4f},// cold taiga hills
-			{0.3f, 0.8f},// mega taiga
-			{0.3f, 0.8f},// mega taiga hills
-			{0.2f, 0.3f},// extreme hills+
-			{1.2f, 0.0f},// savanna
-			{1.0f, 0.0f},// savanna plateau
-			{2.0f, 0.0f},// mesa
-			{2.0f, 0.0f},// mesa plateau f
-			{2.0f, 0.0f},// mesa plateau
-	};
-	/**
-	 * Marker colors for the biome map.
-	 */
-	private static final int[] biomeColor = {
-			0x000070,// ocean
-			0x8DB360,// plains
-			0xFA9418,// desert
-			0x606060,// extreme hills
-			0x056621,// forest
-			0x00DD2D,// taiga
-			0x07F9B2,// swampland
-			0x0000FF,// river
-			0xFF0000,// hell
-			0x8080FF,// sky
-			0x9090A0,// frozen ocean
-			0xA0A0FF,// frozen river
-			0xFFFFFF,// ice plains
-			0xA0A0A0,// ice mountains
-			0xFF00FF,// mushroom island
-			0xA000FF,// mushroom island shore
-			0xFADE55,// beach
-			0xD25F12,// desert hills
-			0x22551C,// forest hills
-			0x163933,// taiga hills
-			0x72789A,// extreme hills edge
-			0x537B09,// jungle
-			0x2C4205,// jungle hills
-			0x628B17,// jungle edge
-			0x000030,// deep ocean
-			0xA2A284,// stone beach
-			0xFAF0C0,// cold beach
-			0x307444,// birch forest
-			0x1F5F32,// birch forest hills
-			0x40511A,// roofed forest
-			0x31554A,// cold taiga
-			0x243F36,// cold taiga hills
-			0x596651,// mega taiga
-			0x454F3E,// mega taiga hills
-			0x507050,// extreme hills+
-			0xBDB25F,// savanna
-			0xA79D64,// savanna plateau
-			0xD94515,// mesa
-			0xB09765,// mesa plateau f
-			0xCA8C65,// mesa plateau
+	private static final Biome biomes[] = {
+		new Biome("ocean", 0.5, 0.5, 0x000070, 0x75B646),
+		new Biome("plains", 0.8, 0.4, 0x8DB360, 0x8DB84A),
+		new Biome("desert", 1.0, 0.0, 0xFA9418, 0x9BA863),
+		new Biome("extreme hills", 0.2, 0.3, 0x606060, 0x75B646),
+		new Biome("forest", 0.7, 0.8, 0x056621, 0x4A8F3A),
+		new Biome("taiga", 0.05, 0.8, 0x00DD2D, 0x478852),
+		new Biome("swampland", 0.8, 0.9, 0x07F9B2, 0x3e5226),
+		new Biome("river", 0.5, 0.5, 0x0000FF, 0x75B646),
+		new Biome("hell", 1.0, 0.0, 0xFF0000, 0x75B646),
+		new Biome("sky", 0.5, 0.5, 0x8080FF, 0x75B646),
+		new Biome("frozen ocean", 0.0, 0.5, 0x9090A0, 0x7A9C91),
+		new Biome("frozen river", 0.0, 0.5, 0xA0A0FF, 0x7A9C91),
+		new Biome("ice plains", 0.0, 0.5, 0xFFFFFF, 0x7A9C91),
+		new Biome("ice mountains", 0.0, 0.5, 0xA0A0A0, 0x7A9C91),
+		new Biome("mushroom island", 0.9, 1.0, 0xFF00FF, 0x939D88),
+		new Biome("mushroom island shore", 0.9, 1.0, 0xA000FF, 0x939D88),
+		new Biome("beach", 0.8, 0.4, 0xFADE55, 0x75B646),
+		new Biome("desert hills", 1.0, 0.0, 0xD25F12, 0x9BA863),
+		new Biome("forest hills", 0.7, 0.8, 0x22551C, 0x4A8F3A),
+		new Biome("taiga hills", 0.05,0.8, 0x163933, 0x478852),
+		new Biome("extreme hills edge", 0.2, 0.3, 0x72789A, 0x75B646),
+		new Biome("jungle", 1.0, 0.9, 0x537B09, 0x3A8B25),
+		new Biome("jungle hills", 1.0, 0.9, 0x2C4205, 0x3A8B25),
+		new Biome("jungle edge", 0.95, 0.8, 0x628B17, 0x3EB80F),
+		new Biome("deep ocean", 0.5, 0.5, 0x000030, 0x71A74D),
+		new Biome("stone beach", 0.2, 0.3, 0xA2A284, 0x6DA36B),
+		new Biome("cold beach", 0.05, 0.3, 0xFAF0C0, 0x64A278),
+		new Biome("birch forest", 0.7, 0.8, 0x307444, 0x59AE30),
+		new Biome("birch forest hills", 0.7, 0.8, 0x1F5F32, 0x59AE30),
+		new Biome("roofed forest", 0.7, 0.8, 0x40511A, 0x59AE30),
+		new Biome("cold taiga", -0.5, 0.4, 0x31554A, 0x60A17B),
+		new Biome("cold taiga hills", -0.5, 0.4, 0x243F36, 0x60A17B),
+		new Biome("mega taiga", 0.3, 0.8, 0x596651, 0x68A55F),
+		new Biome("mega taiga hills", 0.3, 0.8, 0x454F3E, 0x68A55F),
+		new Biome("extreme hills+", 0.2, 0.3, 0x507050, 0x6DA36B),
+		new Biome("savanna", 1.2, 0.0, 0xBDB25F, 0xAEA42A),
+		new Biome("savanna plateau", 1.0, 0.0, 0xA79D64, 0xAEA42A),
+		new Biome("mesa", 2.0, 0.0, 0xD94515, 0xAEA42A),
+		new Biome("mesa plateau f", 2.0, 0.0, 0xB09765, 0xAEA42A),
+		new Biome("mesa plateau", 2.0, 0.0, 0xCA8C65, 0xAEA42A),
 	};
 
 	private static int[] grassColor = new int[256];
@@ -128,48 +80,10 @@ public class Biomes {
 	private static final int UNKNOWN_COLOR = 0x7E7E7E;
 
 	static {
-		grassColor[0] = 0x75B646;// ocean
-		grassColor[1] = 0x8DB84A;// plains
-		grassColor[2] = 0x9BA863;// desert
-		grassColor[3] = 0x75B646;// extreme hills
-		grassColor[4] = 0x4A8F3A;// forest
-		grassColor[5] = 0x478852;// taiga
-		grassColor[SWAMP_ID] = 0x3e5226;// swampland
-		grassColor[7] = 0x75B646;// river
-		grassColor[8] = 0x75B646;// hell
-		grassColor[9] = 0x75B646;// sky
-		grassColor[10] = 0x7A9C91;// frozen ocean
-		grassColor[11] = 0x7A9C91;// frozen river
-		grassColor[12] = 0x7A9C91;// ice plains
-		grassColor[13] = 0x7A9C91;// ice mountains
-		grassColor[14] = 0x939D88;// mushroom island
-		grassColor[15] = 0x939D88;// mushroom island shore
-		grassColor[16] = 0x75B646;// beach
-		grassColor[17] = 0x9BA863;// desert hills
-		grassColor[18] = 0x4A8F3A;// forest hills
-		grassColor[19] = 0x478852;// taiga hills
-		grassColor[20] = 0x75B646;// extreme hills edge
-		grassColor[21] = 0x3A8B25;// jungle
-		grassColor[22] = 0x3A8B25;// jungle hills
-		grassColor[23] = 0x3EB80F;// jungle edge
-		grassColor[24] = 0x71A74D;// deep ocean
-		grassColor[25] = 0x6DA36B;// stone beach
-		grassColor[26] = 0x64A278;// cold beach
-		grassColor[27] = 0x59AE30;// birch forest
-		grassColor[28] = 0x59AE30;// birch forest hills
-		grassColor[29] = 0x59AE30;// roofed forest
-		grassColor[30] = 0x60A17B;// cold taiga
-		grassColor[31] = 0x60A17B;// cold taiga hills
-		grassColor[32] = 0x68A55F;// mega taiga
-		grassColor[33] = 0x68A55F;// mega taiga hills
-		grassColor[34] = 0x6DA36B;// extreme hills+
-		grassColor[35] = 0xAEA42A;// savanna
-		grassColor[36] = 0xAEA42A;// savanna plateau
-		grassColor[37] = 0xAEA42A;// mesa
-		grassColor[38] = 0xAEA42A;// mesa plateau f
-		grassColor[39] = 0xAEA42A;// mesa plateau
-
-		for (int i = 40; i < 256; ++i) {
+		for (int i = 0; i < biomes.length; ++i) {
+			grassColor[i] = biomes[i].grassColor;
+		}
+		for (int i = biomes.length; i < 256; ++i) {
 			grassColor[i] = UNKNOWN_COLOR;
 		}
 
@@ -189,9 +103,9 @@ public class Biomes {
 	 * @return Biome color for given biome ID
 	 */
 	public static final int getColor(int biomeId) {
-		if (biomeId >= biomeColor.length)
+		if (biomeId >= biomes.length)
 			return UNKNOWN_COLOR;
-		return biomeColor[biomeId];
+		return biomes[biomeId].mapColor;
 	}
 
 	/**
@@ -213,9 +127,9 @@ public class Biomes {
 	}
 
 	private static void loadColorsFromTexture(int[] dest, BufferedImage texture) {
-		for (int i = 0; i < tempAndRain.length; ++i) {
-			double temp = QuickMath.clamp(tempAndRain[i][0], 0, 1);
-			double rain = QuickMath.clamp(tempAndRain[i][1], 0, 1);
+		for (int i = 0; i < biomes.length; ++i) {
+			double temp = QuickMath.clamp(biomes[i].temp, 0, 1);
+			double rain = QuickMath.clamp(biomes[i].rain, 0, 1);
 			rain *= temp;
 			int	color = texture.getRGB((int) ((1-temp) * 255), (int) ((1-rain) * 255));
 			dest[i] = color;
