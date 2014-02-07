e29629e77c20a6461c8cad42f7d0639def52de97
Austin Bonander <austin.bonander@gmail.com>
2014-02-07 01:10:45 -0800

    + Copyright/license info to ShutdownAlert.java

--- chunky/src/java/se/llbit/chunky/renderer/ui/ShutdownAlert.java
+++ chunky/src/java/se/llbit/chunky/renderer/ui/ShutdownAlert.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2012-2014 Austin Bonander <austin.bonander@gmail.com>
+ *
+ *  This file is part of Chunky.
+ *
+ *  Chunky is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Chunky is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with Chunky.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.llbit.chunky.renderer.ui;
 
 import org.apache.commons.lang3.SystemUtils;
@@ -9,11 +26,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.*;
 
-/**
- * Created by Austin on 2/6/14.
- */
 public class ShutdownAlert extends ProgressMonitor implements ActionListener{
 
 
