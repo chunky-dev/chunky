chunky-dev/chunky

commit 17da5cac453988368b4da481e1e7fe8ab271220e

Allow plugins to modify render controls tabs
Added new plugin API methods:

* Chunky.setRenderControlsTabTransformer()

The asynchronous scene manager now reports a warning if it can't perform
an action because another action is currently running.

Redesigned the render status listeners:

* The task tracker has been separated from render status listeners.
* Frame completion and render completion events use separate listeners.

fixes #391 (github)

chunky/src/java/se/llbit/chunky/ui/RenderControlsFxController.java
@@ -24,7 +24,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
@@ -36,17 +35,16 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import se.llbit.chunky.PersistentSettings;
-import se.llbit.chunky.map.WorldMapLoader;
 import se.llbit.chunky.renderer.OutputMode;
 import se.llbit.chunky.renderer.RenderController;
 import se.llbit.chunky.renderer.RenderMode;
 import se.llbit.chunky.renderer.RenderStatusListener;
 import se.llbit.chunky.renderer.Renderer;
 import se.llbit.chunky.renderer.ResetReason;
+import se.llbit.chunky.renderer.SnapshotControl;
 import se.llbit.chunky.renderer.scene.AsynchronousSceneManager;
 import se.llbit.chunky.renderer.scene.RenderResetHandler;
 import se.llbit.chunky.renderer.scene.Scene;
-import se.llbit.chunky.renderer.StandardRenderListener;
 import se.llbit.chunky.ui.render.AdvancedTab;
 import se.llbit.chunky.ui.render.CameraTab;
 import se.llbit.chunky.ui.render.EntitiesTab;
@@ -54,20 +52,27 @@ import se.llbit.chunky.ui.render.GeneralTab;
 import se.llbit.chunky.ui.render.HelpTab;
 import se.llbit.chunky.ui.render.LightingTab;
 import se.llbit.chunky.ui.render.PostprocessingTab;
-import se.llbit.chunky.ui.render.RenderControlTab;
+import se.llbit.chunky.ui.render.RenderControlsTab;
 import se.llbit.chunky.ui.render.SkyTab;
 import se.llbit.chunky.ui.render.WaterTab;
 import se.llbit.chunky.world.Icon;
 import se.llbit.log.Log;
 import se.llbit.util.ProgressListener;
+import se.llbit.util.TaskTracker;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 /**
  * Controller for the Render Controls dialog.
@@ -75,22 +80,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class RenderControlsFxController implements Initializable, RenderResetHandler {
   private AsynchronousSceneManager asyncSceneManager;
 
-  static class GUIRenderListener extends StandardRenderListener {
+  public ChunkyFxController getChunkyController() {
+    return controller;
+  }
+
+  public RenderController getRenderController() {
+    return controller.getChunky().getRenderController();
+  }
+
+  static class GUIRenderListener implements RenderStatusListener {
     private final RenderControlsFxController gui;
     private int spp;
     private int sps;
 
-    public GUIRenderListener(RenderControlsFxController renderControls, RenderController controller,
-        ProgressListener progressListener) {
-      super(controller.getContext(), controller.getSceneManager(), progressListener);
+    public GUIRenderListener(RenderControlsFxController renderControls) {
       this.gui = renderControls;
-    }
-
-    @Override public void chunksLoaded() {
-      Platform.runLater(() -> {
-        gui.openPreview();
-        gui.cameraTab.chunksLoaded();
-      });
     }
 
     @Override public void setRenderTime(long time) {
@@ -119,30 +123,6 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
               gui.decimalFormat.format(sps))));
     }
 
-    @Override public void sceneSaved() {
-    }
-
-    @Override public void sceneLoaded() {
-      CountDownLatch guiUpdateLatch = new CountDownLatch(1);
-      Platform.runLater(() -> {
-        synchronized (gui.scene) {
-          gui.sceneNameField.setText(gui.scene.name());
-          gui.canvas.setCanvasSize(gui.scene.width, gui.scene.height);
-        }
-        gui.updateTitle();
-        gui.refreshSettings();
-        guiUpdateLatch.countDown();
-      });
-      new Thread(() -> {
-        try {
-          guiUpdateLatch.await();
-          gui.canvas.forceRepaint();
-        } catch (InterruptedException ignored) {
-          // Ignored.
-        }
-      }).start();
-    }
-
     @Override public void renderStateChanged(RenderMode state) {
       Platform.runLater(() -> {
         switch (state) {
@@ -157,13 +137,6 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
             break;
         }
       });
-    }
-
-    @Override public void renderJobFinished(long time, int sps) {
-      // TODO make sure this works.
-      if (gui.advancedTab.shutdownAfterCompletedRender()) {
-        new ShutdownAlert(null);
-      }
     }
   }
 
@@ -181,7 +154,6 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
   private File saveFrameDirectory = new File(System.getProperty("user.dir"));
 
   private Stage stage;
-  private RenderController renderController;
   private RenderCanvasFx canvas;
   private Renderer renderer;
 
@@ -210,6 +182,13 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
 
   private RenderStatusListener renderTracker = RenderStatusListener.NONE;
 
+  private TaskTracker taskTracker = new TaskTracker(ProgressListener.NONE);
+
+  private Collection<RenderControlsTab> tabs = new ArrayList<>();
+
+  /** Maps JavaFX tabs to tab controllers. */
+  private Map<Tab, RenderControlsTab> tabControllers = Collections.emptyMap();
+
   @FXML private TextField sceneNameField;
 
   @FXML private Button saveBtn;
@@ -232,29 +211,12 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
 
   @FXML private Label sppLbl;
 
-  @FXML private GeneralTab generalTab;
-
-  @FXML private LightingTab lightingTab;
-
-  @FXML private SkyTab skyTab;
-
-  @FXML private WaterTab waterTab;
-
-  @FXML private CameraTab cameraTab;
-
-  @FXML private EntitiesTab entitiesTab;
-
-  @FXML private PostprocessingTab postprocessingTab;
-
-  @FXML private AdvancedTab advancedTab;
-
-  @FXML private HelpTab helpTab;
-
   @FXML private IntegerAdjuster targetSpp;
 
   @FXML private Button saveDefaultSpp;
 
   @FXML private TabPane tabPane;
+
   private ChunkyFxController controller;
 
   public RenderControlsFxController() {
@@ -267,9 +229,6 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
     saveBtn.setGraphic(new ImageView(Icon.disk.fxImage()));
     saveBtn.setOnAction(e -> asyncSceneManager.saveScene());
     saveFrameBtn.setOnAction(this::saveCurrentFrame);
-    tabPane.getSelectionModel().selectedItemProperty()
-        .addListener((observable, oldValue, newValue) -> updateTab(newValue));
-    tabPane.getTabs().get(0).setGraphic(new ImageView(Icon.wrench.fxImage()));
     togglePreviewBtn.setOnAction(e -> {
       if (canvas == null || !canvas.isShowing()) {
         openPreview();
@@ -324,7 +283,7 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
         if (!target.getName().endsWith(extension)) {
           target = new File(target.getPath() + extension);
         }
-        scene.saveFrame(target, renderTracker.taskTracker());
+        scene.saveFrame(target, taskTracker);
       } catch (IOException e1) {
         Log.error("Failed to save current frame", e1);
       }
@@ -347,51 +306,41 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
     });
   }
 
-  private void setRenderController(RenderController renderController) {
-    this.renderTracker = new GUIRenderListener(this, renderController, progressListener);
-    this.renderController = renderController;
-    renderer = renderController.getRenderer();
-    scene = renderController.getSceneManager().getScene();
-    sceneNameField.setText(scene.name);
-    sceneNameField.setTextFormatter(new TextFormatter<TextFormatter.Change>(change -> {
-      if (change.isReplaced()) {
-        if (change.getText().isEmpty()) {
-          // Disallow clearing the scene name.
-          change.setText(change.getControlText().substring(change.getRangeStart(),
-              change.getRangeEnd()));
-        }
-      }
-      if (change.isAdded()) {
-        if (!AsynchronousSceneManager.sceneNameIsValid(change.getText())) {
-          // Stop a change adding illegal characters to the scene name.
-          change.setText("");
-        }
-      }
-      return change;
-    }));
-    sceneNameField.textProperty().addListener((observable, oldValue, newValue) -> {
-      scene.setName(newValue);
-      renderController.getSceneProvider().withSceneProtected(scene -> scene.setName(newValue));
-      updateTitle();
-    });
-    sceneNameField.setOnAction(event -> asyncSceneManager.saveScene());
-    generalTab.setRenderController(renderController);
-    lightingTab.setRenderController(renderController);
-    skyTab.setRenderController(renderController);
-    waterTab.setRenderController(renderController);
-    cameraTab.setRenderController(renderController);
-    entitiesTab.setRenderController(renderController);
-    advancedTab.setRenderController(renderController);
-    postprocessingTab.setRenderController(renderController);
-    targetSpp.set(scene.getTargetSpp());
-    targetSpp.onValueChange(value -> scene.setTargetSpp(value));
+  private void buildTabs() {
+    try {
+      // Create the default tabs:
+      tabs.add(new GeneralTab());
+      tabs.add(new LightingTab());
+      tabs.add(new SkyTab());
+      tabs.add(new WaterTab());
+      tabs.add(new CameraTab());
+      tabs.add(new EntitiesTab());
+      tabs.add(new PostprocessingTab());
+      tabs.add(new AdvancedTab());
+      tabs.add(new HelpTab());
 
-    // TODO: remove the cast.
-    asyncSceneManager =
-        (AsynchronousSceneManager) renderController.getSceneManager();
-    asyncSceneManager.setResetHandler(this);
-    asyncSceneManager.setRenderStatusListener(renderTracker);
-    renderer.setRenderListener(renderTracker);
+      // Transform tabs (allows plugin hooks to modify the tabs):
+      tabs = controller.getChunky().getRenderControlsTabTransformer().apply(tabs);
+
+      if (tabs.contains(null)) {
+        Log.error("Null tabs inserted in tab collection (possible plugin error).");
+        tabs = tabs.stream().filter(tab -> tab != null).collect(Collectors.toList());
+      }
+
+      Collection<Tab> javaFxTabs = new ArrayList<>();
+      tabControllers = new HashMap<>();
+      for (RenderControlsTab tab : tabs) {
+        Tab javaFxTab = tab.getTab();
+        tabControllers.put(javaFxTab, tab);
+        javaFxTabs.add(javaFxTab);
+      }
+      tabPane.getTabs().addAll(javaFxTabs);
+    } catch (IOException e) {
+      Log.error("Failed to build render controls tabs.", e);
+    }
+
+    tabPane.getSelectionModel().selectedItemProperty()
+        .addListener((observable, oldValue, newValue) -> updateTab(newValue));
   }
 
   private void refreshSettings() {
@@ -400,8 +349,12 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
   }
 
   private void updateTab(Tab tab) {
-    RenderControlTab controlTab = (RenderControlTab) ((ScrollPane) tab.getContent()).getContent();
-    controlTab.update(scene);
+    RenderControlsTab controller = tabControllers.get(tab);
+    if (controller != null) {
+      tabControllers.get(tab).update(scene);
+    } else {
+      Log.error("Missing tab controller!");
+    }
   }
 
   public void openPreview() {
@@ -431,22 +384,89 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
     canvas.repaint();
   }
 
-  public void setMapLoader(WorldMapLoader mapLoader) {
-    generalTab.setFxController(this);
-    generalTab.setMapLoader(mapLoader);
-    cameraTab.setMapLoader(mapLoader);
-    updateTab(tabPane.getSelectionModel().getSelectedItem());
-  }
-
   public RenderCanvasFx getCanvas() {
     return canvas;
   }
 
   public void setController(ChunkyFxController controller) {
     this.controller = controller;
-    generalTab.setChunkyFxController(controller);
-    setRenderController(controller.getChunky().getRenderController());
-    setMapLoader(controller.getMapLoader());
+    RenderController renderController = controller.getChunky().getRenderController();
+    this.renderTracker = new GUIRenderListener(this);
+    this.taskTracker = new TaskTracker(progressListener);
+    buildTabs();
+    renderer = renderController.getRenderer();
+    scene = renderController.getSceneManager().getScene();
+    sceneNameField.setText(scene.name);
+    sceneNameField.setTextFormatter(new TextFormatter<TextFormatter.Change>(change -> {
+      if (change.isReplaced()) {
+        if (change.getText().isEmpty()) {
+          // Disallow clearing the scene name.
+          change.setText(change.getControlText().substring(change.getRangeStart(),
+              change.getRangeEnd()));
+        }
+      }
+      if (change.isAdded()) {
+        if (!AsynchronousSceneManager.sceneNameIsValid(change.getText())) {
+          // Stop a change adding illegal characters to the scene name.
+          change.setText("");
+        }
+      }
+      return change;
+    }));
+    sceneNameField.textProperty().addListener((observable, oldValue, newValue) -> {
+      scene.setName(newValue);
+      renderController.getSceneProvider().withSceneProtected(scene1 -> scene1.setName(newValue));
+      updateTitle();
+    });
+    sceneNameField.setOnAction(event -> asyncSceneManager.saveScene());
+    targetSpp.set(scene.getTargetSpp());
+    targetSpp.onValueChange(value -> scene.setTargetSpp(value));
+
+    // TODO: remove the cast.
+    asyncSceneManager =
+        (AsynchronousSceneManager) renderController.getSceneManager();
+    asyncSceneManager.setResetHandler(this);
+    asyncSceneManager.setTaskTracker(taskTracker);
+    asyncSceneManager.setOnSceneLoaded(() -> {
+      CountDownLatch guiUpdateLatch = new CountDownLatch(1);
+      Platform.runLater(() -> {
+        synchronized (scene) {
+          sceneNameField.setText(scene.name());
+          canvas.setCanvasSize(scene.width, scene.height);
+        }
+        updateTitle();
+        refreshSettings();
+        guiUpdateLatch.countDown();
+      });
+      new Thread(() -> {
+        try {
+          guiUpdateLatch.await();
+          canvas.forceRepaint();
+        } catch (InterruptedException ignored) {
+          // Ignored.
+        }
+      }).start();
+    });
+    asyncSceneManager.setOnChunksLoaded(() -> Platform.runLater(() -> {
+      openPreview();
+      tabs.forEach(RenderControlsTab::onChunksLoaded);
+    }));
+    asyncSceneManager.setTaskTracker(taskTracker);
+    renderer.setSnapshotControl(SnapshotControl.DEFAULT);
+    renderer.setOnFrameCompleted((scene1, spp) -> {
+      if (SnapshotControl.DEFAULT.saveSnapshot(scene1, spp)) {
+        // Save the current frame.
+        scene1.saveSnapshot(renderController.getContext().getSceneDirectory(), taskTracker);
+      }
+
+      if (SnapshotControl.DEFAULT.saveRenderDump(scene1, spp)) {
+        // Save the scene description and current render dump.
+        asyncSceneManager.saveScene();
+      }
+    });
+    renderer.addRenderListener(renderTracker);
+    tabs.forEach(tab -> tab.setController(this));
+    updateTab(tabPane.getSelectionModel().getSelectedItem());
   }
 
   private void updateTitle() {

chunky/src/res/se/llbit/chunky/ui/render/SkyTab.fxml
@@ -7,47 +7,52 @@
 <?import javafx.scene.control.TitledPane?>
 <?import javafx.scene.layout.HBox?>
 <?import javafx.scene.layout.VBox?>
+<?import javafx.scene.control.ScrollPane?>
 <?import se.llbit.chunky.ui.DoubleAdjuster?>
-
 <?import se.llbit.chunky.ui.SimpleColorPicker?>
-<fx:root spacing="10.0" type="javafx.scene.layout.VBox" xmlns="http://javafx.com/javafx/8.0.40" xmlns:fx="http://javafx.com/fxml/1">
-    <children>
+
+<fx:root text="Sky &amp; Lighting" type="javafx.scene.control.Tab" xmlns="http://javafx.com/javafx/8.0.40" xmlns:fx="http://javafx.com/fxml/1">
+  <ScrollPane>
+    <VBox spacing="10.0">
+      <children>
         <HBox alignment="CENTER_LEFT" prefWidth="200.0" spacing="10.0">
-         <children>
+          <children>
             <Label text="Sky mode:" />
             <ChoiceBox fx:id="skyMode" prefWidth="150.0" />
-         </children>
+          </children>
         </HBox>
-      <TitledPane fx:id="detailsPane" maxHeight="1.7976931348623157E308" maxWidth="1.7976931348623157E308" text="Sky mode settings">
-         <VBox.margin>
+        <TitledPane fx:id="detailsPane" maxHeight="1.7976931348623157E308" maxWidth="1.7976931348623157E308" text="Sky mode settings">
+          <VBox.margin>
             <Insets />
-         </VBox.margin>
-         <content>
+          </VBox.margin>
+          <content>
             <VBox fx:id="skyModeSettings">
-               <padding>
-                  <Insets bottom="10.0" left="10.0" right="10.0" top="10.0" />
-               </padding>
-               <children>
-                  <Label text="Selected mode has no settings." />
-               </children>
+              <padding>
+                <Insets bottom="10.0" left="10.0" right="10.0" top="10.0" />
+              </padding>
+              <children>
+                <Label text="Selected mode has no settings." />
+              </children>
             </VBox>
-         </content></TitledPane>
+          </content></TitledPane>
         <CheckBox fx:id="transparentSkyEnabled" mnemonicParsing="false" text="Transparent sky" />
         <CheckBox fx:id="cloudsEnabled" mnemonicParsing="false" text="Enable clouds" />
-      <DoubleAdjuster fx:id="cloudSize" />
-      <DoubleAdjuster fx:id="cloudX" />
-      <DoubleAdjuster fx:id="cloudY" />
-      <DoubleAdjuster fx:id="cloudZ" />
-      <DoubleAdjuster fx:id="fogDensity" />
-      <HBox alignment="CENTER_LEFT" spacing="10.0">
-         <children>
+        <DoubleAdjuster fx:id="cloudSize" />
+        <DoubleAdjuster fx:id="cloudX" />
+        <DoubleAdjuster fx:id="cloudY" />
+        <DoubleAdjuster fx:id="cloudZ" />
+        <DoubleAdjuster fx:id="fogDensity" />
+        <HBox alignment="CENTER_LEFT" spacing="10.0">
+          <children>
             <Label text="Fog color:" />
             <SimpleColorPicker fx:id="fogColor" />
-         </children>
-      </HBox>
-      <Label text="Hint: Set fog density &gt; 0.1 for thick fog, and &lt; 0.1 for haze effect." />
-    </children>
-    <padding>
+          </children>
+        </HBox>
+        <Label text="Hint: Set fog density &gt; 0.1 for thick fog, and &lt; 0.1 for haze effect." />
+      </children>
+      <padding>
         <Insets bottom="10.0" left="10.0" right="10.0" top="10.0" />
-    </padding>
+      </padding>
+    </VBox>
+  </ScrollPane>
 </fx:root>

chunky/src/java/se/llbit/chunky/main/Chunky.java
@@ -19,18 +19,16 @@ package se.llbit.chunky.main;
 import se.llbit.chunky.PersistentSettings;
 import se.llbit.chunky.Plugin;
 import se.llbit.chunky.renderer.ConsoleProgressListener;
-import se.llbit.chunky.renderer.ConsoleRenderListener;
 import se.llbit.chunky.renderer.OutputMode;
 import se.llbit.chunky.renderer.RayTracerFactory;
 import se.llbit.chunky.renderer.RenderContext;
 import se.llbit.chunky.renderer.RenderContextFactory;
 import se.llbit.chunky.renderer.RenderController;
 import se.llbit.chunky.renderer.RenderManager;
-import se.llbit.chunky.renderer.RenderStatusListener;
 import se.llbit.chunky.renderer.Renderer;
 import se.llbit.chunky.renderer.RendererFactory;
 import se.llbit.chunky.renderer.SceneProvider;
-import se.llbit.chunky.renderer.SimpleRenderListener;
+import se.llbit.chunky.renderer.SnapshotControl;
 import se.llbit.chunky.renderer.scene.AsynchronousSceneManager;
 import se.llbit.chunky.renderer.scene.PathTracer;
 import se.llbit.chunky.renderer.scene.PreviewRayTracer;
@@ -42,11 +40,12 @@ import se.llbit.chunky.renderer.scene.SynchronousSceneManager;
 import se.llbit.chunky.resources.MinecraftFinder;
 import se.llbit.chunky.resources.TexturePackLoader;
 import se.llbit.chunky.ui.ChunkyFx;
-import se.llbit.chunky.world.Block;
+import se.llbit.chunky.ui.render.RenderControlsTabTransformer;
 import se.llbit.json.JsonValue;
 import se.llbit.log.Level;
 import se.llbit.log.Log;
 import se.llbit.log.Receiver;
+import se.llbit.util.TaskTracker;
 import se.llbit.util.Util;
 
 import java.io.File;
@@ -55,6 +54,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Collection;
+import java.util.function.Function;
 
 /**
  * Chunky is a Minecraft mapping and rendering tool created by
@@ -84,6 +85,7 @@ public class Chunky {
   private RendererFactory rendererFactory = RenderManager::new;
   private RayTracerFactory previewRayTracerFactory = PreviewRayTracer::new;
   private RayTracerFactory rayTracerFactory = PathTracer::new;
+  private RenderControlsTabTransformer renderControlsTabTransformer = tabs -> tabs;
 
   /**
    * @return The name of this application, including version string.
@@ -111,9 +113,42 @@ public class Chunky {
     Renderer renderer = rendererFactory.newRenderer(context, true);
     SynchronousSceneManager sceneManager = new SynchronousSceneManager(context, renderer);
     renderer.setSceneProvider(sceneManager);
-    RenderStatusListener renderListener = new ConsoleRenderListener(context, sceneManager);
-    sceneManager.setRenderStatusListener(renderListener);
-    renderer.setRenderListener(renderListener);
+    TaskTracker taskTracker = new TaskTracker(new ConsoleProgressListener(),
+        (tracker, previous, name, size) -> new TaskTracker.Task(tracker, previous, name, size) {
+          @Override public void close() {
+            super.close();
+            long endTime = System.currentTimeMillis();
+            int seconds = (int) ((endTime - startTime) / 1000);
+            System.out.format("\r%s took %dm %ds%n", name, seconds / 60, seconds % 60);
+          }
+        });
+    sceneManager.setTaskTracker(taskTracker);
+    renderer.setSnapshotControl(SnapshotControl.DEFAULT);
+    renderer.setOnFrameCompleted((scene, spp) -> {
+      if (SnapshotControl.DEFAULT.saveSnapshot(scene, spp)) {
+        // Save the current frame.
+        scene.saveSnapshot(context.getSceneDirectory(), taskTracker);
+      }
+
+      if (SnapshotControl.DEFAULT.saveRenderDump(scene, spp)) {
+        // Save the scene description and current render dump.
+        try {
+          sceneManager.saveScene();
+        } catch (InterruptedException e) {
+          throw new Error(e);
+        }
+      }
+    });
+    renderer.setRenderTask(taskTracker.backgroundTask());
+    renderer.setOnRenderCompleted((time, sps) -> {
+      System.out.println("Render job finished.");
+      int seconds = (int) ((time / 1000) % 60);
+      int minutes = (int) ((time / 60000) % 60);
+      int hours = (int) (time / 3600000);
+      System.out.println(String
+          .format("Total rendering time: %d hours, %d minutes, %d seconds", hours, minutes, seconds));
+      System.out.println("Average samples per second (SPS): " + sps);
+    });
 
     try {
       sceneManager.loadScene(options.sceneName);
@@ -256,9 +291,15 @@ public class Chunky {
       try (FileInputStream in = new FileInputStream(file)) {
         scene.loadDescription(in); // Load description to get current SPP & canvas size.
         RenderContext context = new RenderContext(this);
-        SimpleRenderListener listener = new SimpleRenderListener(new ConsoleProgressListener());
+        TaskTracker taskTracker = new TaskTracker(new ConsoleProgressListener(),
+            TaskTracker.Task::new,
+            (tracker, previous, name, size) -> new TaskTracker.Task(tracker, previous, name, size) {
+              @Override public void update() {
+                // Don't report task state to progress listener.
+              }
+            });
         scene.initBuffers();  // Initialize the sample buffer.
-        scene.loadDump(context, listener); // Load the render dump.
+        scene.loadDump(context, taskTracker); // Load the render dump.
         OutputMode outputMode = scene.getOutputMode();
         if (options.imageOutputFile.isEmpty()) {
           String extension = ".png";
@@ -275,7 +316,7 @@ public class Chunky {
             System.out.println("Image output mode: TIFF32");
             break;
         }
-        scene.saveFrame(new File(options.imageOutputFile), listener.taskTracker());
+        scene.saveFrame(new File(options.imageOutputFile), taskTracker);
         System.out.println("Saved snapshot to " + options.imageOutputFile);
         return 0;
       }
@@ -341,5 +382,14 @@ public class Chunky {
 
   public RayTracerFactory getRayTracerFactory() {
     return rayTracerFactory;
+  }
+
+  public void setRenderControlsTabTransformer(
+      RenderControlsTabTransformer renderControlsTabTransformer) {
+    this.renderControlsTabTransformer = renderControlsTabTransformer;
+  }
+
+  public RenderControlsTabTransformer getRenderControlsTabTransformer() {
+    return renderControlsTabTransformer;
   }
 }

chunky/src/java/se/llbit/chunky/main/CommandLineOptions.java
@@ -20,10 +20,8 @@ import org.jastadd.util.PrettyPrinter;
 import se.llbit.chunky.PersistentSettings;
 import se.llbit.chunky.renderer.ConsoleProgressListener;
 import se.llbit.chunky.renderer.RenderContext;
-import se.llbit.chunky.renderer.SimpleRenderListener;
 import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.renderer.scene.SceneDescription;
-import se.llbit.chunky.resources.MinecraftFinder;
 import se.llbit.chunky.resources.TexturePackLoader;
 import se.llbit.chunky.resources.TexturePackLoader.TextureLoadingError;
 import se.llbit.json.JsonNumber;
@@ -35,6 +33,7 @@ import se.llbit.json.JsonValue;
 import se.llbit.log.Log;
 import se.llbit.util.MCDownloader;
 import se.llbit.util.StringUtil;
+import se.llbit.util.TaskTracker;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -351,12 +350,18 @@ public class CommandLineOptions {
         }
         scene.initBuffers();
         RenderContext context = new RenderContext(new Chunky(options));
-        SimpleRenderListener listener = new SimpleRenderListener(new ConsoleProgressListener());
-        scene.loadDump(context, listener); // Load the render dump.
+        TaskTracker taskTracker = new TaskTracker(new ConsoleProgressListener(),
+            TaskTracker.Task::new,
+            (tracker, previous, name, size) -> new TaskTracker.Task(tracker, previous, name, size) {
+              @Override public void update() {
+                // Don't report task state to progress listener.
+              }
+            });
+        scene.loadDump(context, taskTracker); // Load the render dump.
         Log.info("Original scene SPP: " + scene.spp);
-        scene.mergeDump(dumpfile, listener);
+        scene.mergeDump(dumpfile, taskTracker);
         Log.info("Current scene SPP: " + scene.spp);
-        scene.saveDump(context, listener.taskTracker());
+        scene.saveDump(context, taskTracker);
         try (FileOutputStream out = new FileOutputStream(sceneFile)) {
           scene.saveDescription(out);
         }

chunky/src/java/se/llbit/chunky/main/ZipExportJob.java
@@ -50,7 +50,7 @@ public class ZipExportJob extends Thread {
     this.progress = progress;
   }
 
-  public void run() {
+  @Override public void run() {
     if (progress.tryStartJob()) {
       try {
         progress.setJobName("Zip Export");

chunky/src/java/se/llbit/chunky/model/BeaconModel.java
@@ -33,9 +33,8 @@ public class BeaconModel {
   private static final Texture[] tex = {Texture.glass, Texture.beacon, Texture.obsidian,};
 
   /**
-   * Find intersection between ray and block
+   * Find intersection between ray and block.
    *
-   * @param ray
    * @return <code>true</code> if the ray intersected the block
    */
   public static boolean intersect(Ray ray) {

commit 453b1e50d78b9e7df2587e8034feefaed68b2ae0

Fix render progress tracker not working
Fixed bug introduced in 17da5cac453988368b4da481e1e7fe8ab271220e due to
refactoring the RenderStatusListener interface. The progress bar was not
updated because its listener was no longer registered with the renderer.

Also changed the title of the sky tab back to "Sky & Fog".

chunky/src/java/se/llbit/chunky/ui/RenderControlsFxController.java
@@ -465,6 +465,7 @@ public class RenderControlsFxController implements Initializable, RenderResetHan
       }
     });
     renderer.addRenderListener(renderTracker);
+    renderer.setRenderTask(taskTracker.backgroundTask());
     tabs.forEach(tab -> tab.setController(this));
     updateTab(tabPane.getSelectionModel().getSelectedItem());
   }

chunky/src/res/se/llbit/chunky/ui/render/SkyTab.fxml
@@ -11,7 +11,7 @@
 <?import se.llbit.chunky.ui.DoubleAdjuster?>
 <?import se.llbit.chunky.ui.SimpleColorPicker?>
 
-<fx:root text="Sky &amp; Lighting" type="javafx.scene.control.Tab" xmlns="http://javafx.com/javafx/8.0.40" xmlns:fx="http://javafx.com/fxml/1">
+<fx:root text="Sky &amp; Fog" type="javafx.scene.control.Tab" xmlns="http://javafx.com/javafx/8.0.40" xmlns:fx="http://javafx.com/fxml/1">
   <ScrollPane>
     <VBox spacing="10.0">
       <children>