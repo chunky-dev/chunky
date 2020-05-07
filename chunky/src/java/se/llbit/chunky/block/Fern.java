5c36db721a834e6c9a7246e291f91db97f16a346
Maik Marschner <maik.marschner@maiksite.de>
2020-05-08 00:24:40 +0200

    Fix blocks below fern being rendered as stone. (#580)

--- chunky/src/java/se/llbit/chunky/block/Fern.java
+++ chunky/src/java/se/llbit/chunky/block/Fern.java
@@ -5,7 +5,7 @@ import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Ray;
 
-public class Fern extends MinecraftBlock {
+public class Fern extends MinecraftBlockTranslucent {
   public Fern() {
     super("fern", Texture.fern);
     localIntersect = true;
