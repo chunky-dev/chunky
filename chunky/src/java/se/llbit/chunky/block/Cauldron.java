ba7cd8e81531d6b5a8268fc4b027468be42fc657
Maik Marschner <an@maik.tk>
2020-05-10 17:01:22 +0200

    Render water levels of cauldrons.

--- chunky/src/java/se/llbit/chunky/block/Cauldron.java
+++ chunky/src/java/se/llbit/chunky/block/Cauldron.java
@@ -5,7 +5,6 @@ import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Ray;
 
-// TODO: render water level
 public class Cauldron extends MinecraftBlockTranslucent {
   private final int level;
 
@@ -15,11 +14,13 @@ public class Cauldron extends MinecraftBlockTranslucent {
     localIntersect = true;
   }
 
-  @Override public boolean intersect(Ray ray, Scene scene) {
-    return CauldronModel.intersect(ray);
+  @Override
+  public boolean intersect(Ray ray, Scene scene) {
+    return CauldronModel.intersect(ray, scene.stillWaterEnabled(), level);
   }
 
-  @Override public String description() {
+  @Override
+  public String description() {
     return "level=" + level;
   }
 }
