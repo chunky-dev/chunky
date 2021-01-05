4330c8de5048126678add36bc26d1d2309c3379e
Maik Marschner <an@maik.tk>
2021-01-06 00:55:05 +0100

    Fix texture orientation of dispenser, dropper and furnace.

--- chunky/src/java/se/llbit/chunky/block/Dispenser.java
+++ chunky/src/java/se/llbit/chunky/block/Dispenser.java
@@ -1,79 +1,50 @@
 package se.llbit.chunky.block;
 
-import se.llbit.chunky.model.TexturedBlockModel;
-import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
-import se.llbit.math.Ray;
 
-public class Dispenser extends MinecraftBlock {
-  private static final Texture[][] texture = {
-      // Facing down.
-      {
-        Texture.furnaceTop, Texture.furnaceTop, Texture.furnaceTop, Texture.furnaceTop,
-        Texture.furnaceTop, Texture.dispenserFrontVertical
-      },
-      // Facing up.
-      {
-        Texture.furnaceTop, Texture.furnaceTop, Texture.furnaceTop, Texture.furnaceTop,
-        Texture.dispenserFrontVertical, Texture.furnaceTop
-      },
-      // Facing north.
-      {
-        Texture.dispenserFront, Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceSide,
-        Texture.furnaceTop, Texture.furnaceTop,
-      },
-      // Facing south.
-      {
-        Texture.furnaceSide, Texture.dispenserFront, Texture.furnaceSide, Texture.furnaceSide,
-        Texture.furnaceTop, Texture.furnaceTop,
-      },
-      // Facing east.
-      {
-        Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceSide, Texture.dispenserFront,
-        Texture.furnaceTop, Texture.furnaceTop,
-      },
-      // Facing west.
-      {
-        Texture.furnaceSide, Texture.furnaceSide, Texture.dispenserFront, Texture.furnaceSide,
-        Texture.furnaceTop, Texture.furnaceTop,
-      },
+/**
+ * The dispenser behaves almost like a TopBottomOrientedTexturedBlock. If it's facing up or down, it
+ * has different textures (and thus different texture orientation logic).
+ */
+public class Dispenser extends TopBottomOrientedTexturedBlock {
+
+  private static final int[][] verticalUvRotationMap = {
+      {0, 0, 0, 0, 2, 0}, // up
+      {2, 2, 2, 2, 2, 0}, // down
   };
 
-  private final int facing;
+  private static final int[][] verticalTextureOrientationMap = {
+      {4, 4, 4, 4, 0, 4}, // up
+      {4, 4, 4, 4, 4, 0}, // down
+  };
+
   private final String description;
 
   public Dispenser(String facing) {
-    super("dispenser", Texture.dispenserFront);
+    this("dispenser", facing, Texture.dispenserFront, Texture.dispenserFrontVertical,
+        Texture.furnaceSide, Texture.furnaceTop);
+  }
+
+  public Dispenser(String name, String facing, Texture front, Texture frontVertical, Texture side,
+      Texture back) {
+    super(name, facing,
+        facing.equals("up") || facing.equals("down") ? frontVertical : front,
+        side, back);
     this.description = "facing=" + facing;
-    localIntersect = true;
-    switch (facing) {
-      case "down":
-        this.facing = 0;
-        break;
-      case "up":
-        this.facing = 1;
-        break;
-      default:
-      case "north":
-        this.facing = 2;
-        break;
-      case "south":
-        this.facing = 3;
-        break;
-      case "west":
-        this.facing = 4;
-        break;
-      case "east":
-        this.facing = 5;
-        break;
-    }
   }
 
-  @Override public boolean intersect(Ray ray, Scene scene) {
-    return TexturedBlockModel.intersect(ray, texture[facing]);
-  }
-
-  @Override public String description() {
+  @Override
+  public String description() {
     return description;
+  }
+
+  @Override
+  protected int[][] getUvRotationMap() {
+    return facing <= 1 ? verticalUvRotationMap : super.getUvRotationMap();
+  }
+
+  @Override
+  protected int[][] getTextureOrientationMap() {
+    return facing <= 1 ? verticalTextureOrientationMap : super.getTextureOrientationMap();
   }
 }
--- chunky/src/java/se/llbit/chunky/block/Furnace.java
+++ chunky/src/java/se/llbit/chunky/block/Furnace.java
@@ -1,94 +1,25 @@
 package se.llbit.chunky.block;
 
-import se.llbit.chunky.model.TexturedBlockModel;
-import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
-import se.llbit.math.Ray;
 
-public class Furnace extends MinecraftBlock {
-  private static final Texture[][][] texture = {
-      // Unlit.
-      {
-          // Facing north.
-          {
-              Texture.furnaceUnlitFront, Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceSide,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-          // Facing south.
-          {
-              Texture.furnaceSide, Texture.furnaceUnlitFront, Texture.furnaceSide, Texture.furnaceSide,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-          // Facing east.
-          {
-              Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceUnlitFront,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-          // Facing west.
-          {
-              Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceUnlitFront, Texture.furnaceSide,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-      },
-      // Unlit.
-      {
-          // Facing north.
-          {
-              Texture.furnaceLitFront, Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceSide,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-          // Facing south.
-          {
-              Texture.furnaceSide, Texture.furnaceLitFront, Texture.furnaceSide, Texture.furnaceSide,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-          // Facing east.
-          {
-              Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceLitFront,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-          // Facing west.
-          {
-              Texture.furnaceSide, Texture.furnaceSide, Texture.furnaceLitFront, Texture.furnaceSide,
-              Texture.furnaceTop, Texture.furnaceTop,
-          },
-      }
-  };
+public class Furnace extends TopBottomOrientedTexturedBlock {
 
-  private final int facing, lit;
+  private final boolean lit;
   private final String description;
 
   public Furnace(String facing, boolean lit) {
-    super("furnace", Texture.furnaceLitFront);
+    super("furnace", facing, lit ? Texture.furnaceLitFront : Texture.furnaceUnlitFront,
+        Texture.furnaceSide, Texture.furnaceTop);
     this.description = String.format("facing=%s, lit=%s", facing, lit);
-    localIntersect = true;
-    this.lit = lit ? 1 : 0;
-    switch (facing) {
-      default:
-      case "north":
-        this.facing = 0;
-        break;
-      case "south":
-        this.facing = 1;
-        break;
-      case "west":
-        this.facing = 2;
-        break;
-      case "east":
-        this.facing = 3;
-        break;
-    }
+    this.lit = lit;
   }
 
   public boolean isLit() {
-    return lit > 0;
+    return lit;
   }
 
-  @Override public boolean intersect(Ray ray, Scene scene) {
-    return TexturedBlockModel.intersect(ray, texture[lit][facing]);
-  }
-
-  @Override public String description() {
+  @Override
+  public String description() {
     return description;
   }
 }
