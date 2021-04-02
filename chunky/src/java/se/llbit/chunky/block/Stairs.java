3f80b8be2a79944f5526314f7dbf9373243e44a9
Maik Marschner <an@maik.tk>
2021-04-02 21:50:31 +0200

    Add an interface for blocks that have reusable models.

--- chunky/src/java/se/llbit/chunky/block/Stairs.java
+++ chunky/src/java/se/llbit/chunky/block/Stairs.java
@@ -1,11 +1,12 @@
 package se.llbit.chunky.block;
 
+import se.llbit.chunky.model.BlockModel;
 import se.llbit.chunky.model.StairModel;
 import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Ray;
 
-public class Stairs extends MinecraftBlockTranslucent {
+public class Stairs extends MinecraftBlockTranslucent implements ModelBlock {
   private final int flipped, facing, corner;
   private final boolean isCorner;
   private final Texture side, top, bottom;
@@ -126,5 +127,10 @@ public class Stairs extends MinecraftBlockTranslucent {
 
   @Override public String description() {
     return description;
+  }
+
+  @Override
+  public BlockModel getModel() {
+    return model;
   }
 }
