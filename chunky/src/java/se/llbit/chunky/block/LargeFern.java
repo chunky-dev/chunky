f536f85396760a3b632ca8efc54756e613cf8bb1
Maik Marschner <an@maik.tk>
2021-04-02 22:09:10 +0200

    Make sprite-based blocks implement ModelBlock (e.g. grass, flowers, fern).

--- chunky/src/java/se/llbit/chunky/block/LargeFern.java
+++ chunky/src/java/se/llbit/chunky/block/LargeFern.java
@@ -1,11 +1,15 @@
 package se.llbit.chunky.block;
 
+import se.llbit.chunky.model.BlockModel;
 import se.llbit.chunky.model.TallGrassModel;
 import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Ray;
 
-public class LargeFern extends MinecraftBlockTranslucent {
+public class LargeFern extends MinecraftBlockTranslucent implements ModelBlock {
+
+  private final TallGrassModel model;
+
   public LargeFern(String half) {
     super("large_fern",
         half.equals("upper")
@@ -13,9 +17,16 @@ public class LargeFern extends MinecraftBlockTranslucent {
             : Texture.largeFernBottom);
     localIntersect = true;
     solid = false;
+    model = new TallGrassModel(texture);
   }
 
-  @Override public boolean intersect(Ray ray, Scene scene) {
-    return TallGrassModel.intersect(ray, scene, texture);
+  @Override
+  public boolean intersect(Ray ray, Scene scene) {
+    return model.intersect(ray, scene);
+  }
+
+  @Override
+  public BlockModel getModel() {
+    return model;
   }
 }
