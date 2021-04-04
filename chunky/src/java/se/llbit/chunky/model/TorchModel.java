5737d28f9ca3856e2b4fc76a36a11e96ada65996
Maik Marschner <an@maik.tk>
2021-04-04 23:51:01 +0200

    Refactor the torch model to use quads only.

--- chunky/src/java/se/llbit/chunky/model/TorchModel.java
+++ chunky/src/java/se/llbit/chunky/model/TorchModel.java
@@ -18,96 +18,97 @@ package se.llbit.chunky.model;
 
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Quad;
-import se.llbit.math.QuickMath;
 import se.llbit.math.Ray;
-import se.llbit.math.UVTriangle;
-import se.llbit.math.Vector2;
 import se.llbit.math.Vector3;
 import se.llbit.math.Vector4;
 
 public class TorchModel {
-  // facing west
-  private static final Quad[] quads = {
-      // west
-      new Quad(new Vector3(15 / 16., 3 / 16., 0), new Vector3(15 / 16., 3 / 16., 1),
-          new Vector3((11 - 12 / 10.) / 16., 1, 0), new Vector4(0, 1, 0, 13 / 16.)),
 
-      // east
-      new Quad(new Vector3((13 - 12 / 10.) / 16., 1, 0), new Vector3((13 - 12 / 10.) / 16., 1, 1),
-          new Vector3(17 / 16., 3 / 16., 0), new Vector4(1, 0, 13 / 16., 0)),
+  private static final Quad[] quadsGround = new Quad[]{
+      new Quad(
+          new Vector3(7 / 16.0, 10 / 16.0, 9 / 16.0),
+          new Vector3(9 / 16.0, 10 / 16.0, 9 / 16.0),
+          new Vector3(7 / 16.0, 10 / 16.0, 7 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 8 / 16.0, 10 / 16.0)
+      ),
+      new Quad(
+          new Vector3(7 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector3(7 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 1 / 16.0, 3 / 16.0)
+      ),
+      new Quad(
+          new Vector3(7 / 16.0, 16 / 16.0, 16 / 16.0),
+          new Vector3(7 / 16.0, 16 / 16.0, 0 / 16.0),
+          new Vector3(7 / 16.0, 0 / 16.0, 16 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 16 / 16.0, 0 / 16.0),
+          new Vector3(9 / 16.0, 16 / 16.0, 16 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 0 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(0 / 16.0, 16 / 16.0, 7 / 16.0),
+          new Vector3(16 / 16.0, 16 / 16.0, 7 / 16.0),
+          new Vector3(0 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(16 / 16.0, 16 / 16.0, 9 / 16.0),
+          new Vector3(0 / 16.0, 16 / 16.0, 9 / 16.0),
+          new Vector3(16 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      )
+  };
 
-      // top
-      new Quad(new Vector3(13 / 16., 13 / 16., 9 / 16.), new Vector3(13 / 16., 13 / 16., 7 / 16.),
-          new Vector3(11 / 16., 13 / 16., 9 / 16.),
-          new Vector4(9 / 16., 7 / 16., 10 / 16., 8 / 16.)),
+  private static final Quad[] quadsWall = Model.rotateZ(new Quad[]{
+      new Quad(
+          new Vector3(-1 / 16.0, 13.5 / 16.0, 9 / 16.0),
+          new Vector3(1 / 16.0, 13.5 / 16.0, 9 / 16.0),
+          new Vector3(-1 / 16.0, 13.5 / 16.0, 7 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 8 / 16.0, 10 / 16.0)
+      ),
+      new Quad(
+          new Vector3(-1 / 16.0, 3.5 / 16.0, 7 / 16.0),
+          new Vector3(1 / 16.0, 3.5 / 16.0, 7 / 16.0),
+          new Vector3(-1 / 16.0, 3.5 / 16.0, 9 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 1 / 16.0, 3 / 16.0)
+      ),
+      new Quad(
+          new Vector3(-1 / 16.0, 19.5 / 16.0, 16 / 16.0),
+          new Vector3(-1 / 16.0, 19.5 / 16.0, 0 / 16.0),
+          new Vector3(-1 / 16.0, 3.5 / 16.0, 16 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(1 / 16.0, 19.5 / 16.0, 0 / 16.0),
+          new Vector3(1 / 16.0, 19.5 / 16.0, 16 / 16.0),
+          new Vector3(1 / 16.0, 3.5 / 16.0, 0 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(-8 / 16.0, 19.5 / 16.0, 7 / 16.0),
+          new Vector3(8 / 16.0, 19.5 / 16.0, 7 / 16.0),
+          new Vector3(-8 / 16.0, 3.5 / 16.0, 7 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(8 / 16.0, 19.5 / 16.0, 9 / 16.0),
+          new Vector3(-8 / 16.0, 19.5 / 16.0, 9 / 16.0),
+          new Vector3(8 / 16.0, 3.5 / 16.0, 9 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      )
+  }, Math.toRadians(-22.5), new Vector3(0, 3.5 / 16, 8 / 16.));
 
-      // bottom
-      new Quad(new Vector3(15 / 16., 3 / 16., 7 / 16.), new Vector3(17 / 16., 3 / 16., 7 / 16.),
-          new Vector3(15 / 16., 3 / 16., 9 / 16.),
-          new Vector4(7 / 16., 9 / 16., 0 / 16., 2 / 16.))};
-
-  // facing west
-  private static final UVTriangle[] uvtriangles = {
-      // facing south
-      new UVTriangle(new Vector3(8 / 16., 3 / 16., 9 / 16.),
-          new Vector3(24 / 16., 3 / 16., 9 / 16.), new Vector3((4 - 12 / 10.) / 16., 1, 9 / 16.),
-          new Vector2(0, 0), new Vector2(1, 0), new Vector2(0., 13 / 16.)),
-      new UVTriangle(new Vector3((20 - 12 / 10.) / 16., 1, 9 / 16.),
-          new Vector3((4 - 12 / 10.) / 16., 1, 9 / 16.), new Vector3(24 / 16., 3 / 16., 9 / 16.),
-          new Vector2(1, 13 / 16.), new Vector2(0, 13 / 16.), new Vector2(1, 0)),
-
-      // facing north
-      new UVTriangle(new Vector3(24 / 16., 3 / 16., 7 / 16.),
-          new Vector3(8 / 16., 3 / 16., 7 / 16.), new Vector3((4 - 12 / 10.) / 16., 1, 7 / 16.),
-          new Vector2(1, 0), new Vector2(0, 0), new Vector2(0, 13 / 16.)),
-      new UVTriangle(new Vector3((4 - 12 / 10.) / 16., 1, 7 / 16.),
-          new Vector3((20 - 12 / 10.) / 16., 1, 7 / 16.), new Vector3(24 / 16., 3 / 16., 7 / 16.),
-          new Vector2(0, 13 / 16.), new Vector2(1, 13 / 16.), new Vector2(1, 0))};
-
-  private static Quad[] onGround =
-      {new Quad(new Vector3(.75, 0, .4375), new Vector3(.25, 0, .4375),
-          new Vector3(.75, 1, .4375), new Vector4(.75, .25, 0, 1)),
-
-          new Quad(new Vector3(.25, 0, .5625), new Vector3(.75, 0, .5625),
-              new Vector3(.25, 1, .5625), new Vector4(.25, .75, 0, 1)),
-
-          new Quad(new Vector3(.4375, 0, .25), new Vector3(.4375, 0, .75),
-              new Vector3(.4375, 1, .25), new Vector4(.25, .75, 0, 1)),
-
-          new Quad(new Vector3(.5625, 0, .75), new Vector3(.5625, 0, .25),
-              new Vector3(.5625, 1, .75), new Vector4(.75, .25, 0, 1)),
-
-          // top
-          new Quad(new Vector3(.4375, .625, .5625), new Vector3(.5625, .625, .5625),
-              new Vector3(.4375, .625, .4375), new Vector4(.4375, .5625, .5, .625)),};
-
-  private static Quad[][] rotQuads = new Quad[6][];
-  private static UVTriangle[][] rotTriangles = new UVTriangle[6][];
+  private static final Quad[][] rotatedQuadsWall = new Quad[6][];
 
   static {
-    rotQuads[0] = new Quad[0];
-    rotTriangles[0] = new UVTriangle[0];
-
-    // pointing west
-    rotQuads[2] = quads;
-    rotTriangles[2] = uvtriangles;
-
-    // pointing north
-    rotQuads[4] = Model.rotateY(rotQuads[2]);
-    rotTriangles[4] = Model.rotateY(rotTriangles[2]);
-
-    // pointing east
-    rotQuads[1] = Model.rotateY(rotQuads[4]);
-    rotTriangles[1] = Model.rotateY(rotTriangles[4]);
-
-    // pointing south
-    rotQuads[3] = Model.rotateY(rotQuads[1]);
-    rotTriangles[3] = Model.rotateY(rotTriangles[1]);
-
-    // on ground
-    rotQuads[5] = onGround;
-    rotTriangles[5] = new UVTriangle[0];
-
+    rotatedQuadsWall[1] = quadsWall; // east
+    rotatedQuadsWall[3] = Model.rotateY(rotatedQuadsWall[1]); // south
+    rotatedQuadsWall[2] = Model.rotateY(rotatedQuadsWall[3]); // west
+    rotatedQuadsWall[4] = Model.rotateY(rotatedQuadsWall[2]); // north
   }
 
   public static boolean intersect(Ray ray, Texture texture) {
@@ -118,40 +119,24 @@ public class TorchModel {
   public static boolean intersect(Ray ray, Texture texture, int rot) {
     boolean hit = false;
     ray.t = Double.POSITIVE_INFINITY;
-    float[] color = null;
-    for (Quad quad : rotQuads[rot]) {
+    float[] color;
+    Quad[] quads = rot < 5 ? rotatedQuadsWall[rot] : quadsGround;
+    for (Quad quad : quads) {
       if (quad.intersect(ray)) {
-        float[] c = texture.getColor(ray.u, ray.v);
-        if (c[3] > Ray.EPSILON) {
-          color = c;
+        color = texture.getColor(ray.u, ray.v);
+        if (color[3] > Ray.EPSILON) {
+          ray.color.set(color);
           ray.n.set(quad.n);
           ray.t = ray.tNext;
           hit = true;
         }
       }
     }
-    for (UVTriangle triangle : rotTriangles[rot]) {
-      if (triangle.intersect(ray)) {
-        float[] c = texture.getColor(ray.u, ray.v);
-        if (c[3] > Ray.EPSILON) {
-          color = c;
-          ray.n.set(triangle.n);
-          ray.t = ray.tNext;
-          hit = true;
-        }
-      }
-    }
+
     if (hit) {
-      double px = ray.o.x - QuickMath.floor(ray.o.x + ray.d.x * Ray.OFFSET) + ray.d.x * ray.tNext;
-      double py = ray.o.y - QuickMath.floor(ray.o.y + ray.d.y * Ray.OFFSET) + ray.d.y * ray.tNext;
-      double pz = ray.o.z - QuickMath.floor(ray.o.z + ray.d.z * Ray.OFFSET) + ray.d.z * ray.tNext;
-      if (px >= 0 && px <= 1 && py >= 0 && py <= 1 && pz >= 0 && pz <= 1) {
-        ray.color.set(color);
-        ray.distance += ray.t;
-        ray.o.scaleAdd(ray.t, ray.d);
-        return true;
-      }
+      ray.distance += ray.t;
+      ray.o.scaleAdd(ray.t, ray.d);
     }
-    return false;
+    return hit;
   }
 }
