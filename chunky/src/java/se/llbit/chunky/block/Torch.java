76586827cb7822f8515b0b543253832ca93a5fd8
Maik Marschner <an@maik.tk>
2021-04-05 17:51:44 +0200

    Refactor torches to model blocks.

--- chunky/src/java/se/llbit/chunky/block/Torch.java
+++ chunky/src/java/se/llbit/chunky/block/Torch.java
@@ -1,5 +1,6 @@
 package se.llbit.chunky.block;
 
+import se.llbit.chunky.model.BlockModel;
 import se.llbit.chunky.model.TorchModel;
 import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
@@ -8,15 +9,25 @@ import se.llbit.math.Ray;
 /**
  * A standing torch (on ground).
  */
-public class Torch extends MinecraftBlock {
+public class Torch extends MinecraftBlock implements ModelBlock {
+
+  private final BlockModel model;
+
   public Torch(String name, Texture texture) {
     super(name, texture);
     localIntersect = true;
     opaque = false;
     solid = false;
+    model = new TorchModel(texture, 5);
   }
 
-  @Override public boolean intersect(Ray ray, Scene scene) {
-    return TorchModel.intersect(ray, texture, 5);
+  @Override
+  public boolean intersect(Ray ray, Scene scene) {
+    return model.intersect(ray, scene);
+  }
+
+  @Override
+  public BlockModel getModel() {
+    return model;
   }
 }
--- chunky/src/java/se/llbit/chunky/block/WallTorch.java
+++ chunky/src/java/se/llbit/chunky/block/WallTorch.java
@@ -1,5 +1,6 @@
 package se.llbit.chunky.block;
 
+import se.llbit.chunky.model.BlockModel;
 import se.llbit.chunky.model.TorchModel;
 import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
@@ -8,9 +9,10 @@ import se.llbit.math.Ray;
 /**
  * A torch attached to a wall.
  */
-public class WallTorch extends MinecraftBlock {
+public class WallTorch extends MinecraftBlock implements ModelBlock {
   private final String description;
   private final int facing;
+  private final BlockModel model;
 
   public WallTorch(String name, Texture texture, String facing) {
     super(name, texture);
@@ -33,13 +35,19 @@ public class WallTorch extends MinecraftBlock {
         this.facing = 1;
         break;
     }
+    model = new TorchModel(texture, this.facing);
   }
 
   @Override public boolean intersect(Ray ray, Scene scene) {
-    return TorchModel.intersect(ray, texture, facing);
+    return model.intersect(ray, scene);
   }
 
   @Override public String description() {
     return description;
+  }
+
+  @Override
+  public BlockModel getModel() {
+    return model;
   }
 }
