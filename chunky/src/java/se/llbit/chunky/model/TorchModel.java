chunky-dev/chunky

commit cb21eadb75e6997167ca7d03d55c8f5418f90de5

Remove the UVTriangle class (#877)
* Refactor the torch model to use quads only.

* Remove the now unused UVTriangle class.

chunky/src/java/se/llbit/chunky/model/TorchModel.java
@@ -18,96 +18,97 @@ package se.llbit.chunky.model;
 
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Quad;
-import se.llbit.math.QuickMath;
 import se.llbit.math.Ray;
-import se.llbit.math.UVTriangle;
-import se.llbit.math.Vector2;
 import se.llbit.math.Vector3;
 import se.llbit.math.Vector4;
 
 public class TorchModel {
-  // facing west
-  private static final Quad[] quads = {
-      // west
-      new Quad(new Vector3(15 / 16., 3 / 16., 0), new Vector3(15 / 16., 3 / 16., 1),
-          new Vector3((11 - 12 / 10.) / 16., 1, 0), new Vector4(0, 1, 0, 13 / 16.)),
 
-      // east
-      new Quad(new Vector3((13 - 12 / 10.) / 16., 1, 0), new Vector3((13 - 12 / 10.) / 16., 1, 1),
-          new Vector3(17 / 16., 3 / 16., 0), new Vector4(1, 0, 13 / 16., 0)),
+  private static final Quad[] quadsGround = new Quad[]{
+      new Quad(
+          new Vector3(7 / 16.0, 10 / 16.0, 9 / 16.0),
+          new Vector3(9 / 16.0, 10 / 16.0, 9 / 16.0),
+          new Vector3(7 / 16.0, 10 / 16.0, 7 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 8 / 16.0, 10 / 16.0)
+      ),
+      new Quad(
+          new Vector3(7 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector3(7 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 1 / 16.0, 3 / 16.0)
+      ),
+      new Quad(
+          new Vector3(7 / 16.0, 16 / 16.0, 16 / 16.0),
+          new Vector3(7 / 16.0, 16 / 16.0, 0 / 16.0),
+          new Vector3(7 / 16.0, 0 / 16.0, 16 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 16 / 16.0, 0 / 16.0),
+          new Vector3(9 / 16.0, 16 / 16.0, 16 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 0 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(0 / 16.0, 16 / 16.0, 7 / 16.0),
+          new Vector3(16 / 16.0, 16 / 16.0, 7 / 16.0),
+          new Vector3(0 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(16 / 16.0, 16 / 16.0, 9 / 16.0),
+          new Vector3(0 / 16.0, 16 / 16.0, 9 / 16.0),
+          new Vector3(16 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      )
+  };
 
-      // top
-      new Quad(new Vector3(13 / 16., 13 / 16., 9 / 16.), new Vector3(13 / 16., 13 / 16., 7 / 16.),
-          new Vector3(11 / 16., 13 / 16., 9 / 16.),
-          new Vector4(9 / 16., 7 / 16., 10 / 16., 8 / 16.)),
+  private static final Quad[] quadsWall = Model.rotateZ(new Quad[]{
+      new Quad(
+          new Vector3(-1 / 16.0, 13.5 / 16.0, 9 / 16.0),
+          new Vector3(1 / 16.0, 13.5 / 16.0, 9 / 16.0),
+          new Vector3(-1 / 16.0, 13.5 / 16.0, 7 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 8 / 16.0, 10 / 16.0)
+      ),
+      new Quad(
+          new Vector3(-1 / 16.0, 3.5 / 16.0, 7 / 16.0),
+          new Vector3(1 / 16.0, 3.5 / 16.0, 7 / 16.0),
+          new Vector3(-1 / 16.0, 3.5 / 16.0, 9 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 1 / 16.0, 3 / 16.0)
+      ),
+      new Quad(
+          new Vector3(-1 / 16.0, 19.5 / 16.0, 16 / 16.0),
+          new Vector3(-1 / 16.0, 19.5 / 16.0, 0 / 16.0),
+          new Vector3(-1 / 16.0, 3.5 / 16.0, 16 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(1 / 16.0, 19.5 / 16.0, 0 / 16.0),
+          new Vector3(1 / 16.0, 19.5 / 16.0, 16 / 16.0),
+          new Vector3(1 / 16.0, 3.5 / 16.0, 0 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(-8 / 16.0, 19.5 / 16.0, 7 / 16.0),
+          new Vector3(8 / 16.0, 19.5 / 16.0, 7 / 16.0),
+          new Vector3(-8 / 16.0, 3.5 / 16.0, 7 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(8 / 16.0, 19.5 / 16.0, 9 / 16.0),
+          new Vector3(-8 / 16.0, 19.5 / 16.0, 9 / 16.0),
+          new Vector3(8 / 16.0, 3.5 / 16.0, 9 / 16.0),
+          new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
+      )
+  }, Math.toRadians(-22.5), new Vector3(0, 3.5 / 16, 8 / 16.));
 
-      // bottom
-      new Quad(new Vector3(15 / 16., 3 / 16., 7 / 16.), new Vector3(17 / 16., 3 / 16., 7 / 16.),
-          new Vector3(15 / 16., 3 / 16., 9 / 16.),
-          new Vector4(7 / 16., 9 / 16., 0 / 16., 2 / 16.))};
-
-  // facing west
-  private static final UVTriangle[] uvtriangles = {
-      // facing south
-      new UVTriangle(new Vector3(8 / 16., 3 / 16., 9 / 16.),
-          new Vector3(24 / 16., 3 / 16., 9 / 16.), new Vector3((4 - 12 / 10.) / 16., 1, 9 / 16.),
-          new Vector2(0, 0), new Vector2(1, 0), new Vector2(0., 13 / 16.)),
-      new UVTriangle(new Vector3((20 - 12 / 10.) / 16., 1, 9 / 16.),
-          new Vector3((4 - 12 / 10.) / 16., 1, 9 / 16.), new Vector3(24 / 16., 3 / 16., 9 / 16.),
-          new Vector2(1, 13 / 16.), new Vector2(0, 13 / 16.), new Vector2(1, 0)),
-
-      // facing north
-      new UVTriangle(new Vector3(24 / 16., 3 / 16., 7 / 16.),
-          new Vector3(8 / 16., 3 / 16., 7 / 16.), new Vector3((4 - 12 / 10.) / 16., 1, 7 / 16.),
-          new Vector2(1, 0), new Vector2(0, 0), new Vector2(0, 13 / 16.)),
-      new UVTriangle(new Vector3((4 - 12 / 10.) / 16., 1, 7 / 16.),
-          new Vector3((20 - 12 / 10.) / 16., 1, 7 / 16.), new Vector3(24 / 16., 3 / 16., 7 / 16.),
-          new Vector2(0, 13 / 16.), new Vector2(1, 13 / 16.), new Vector2(1, 0))};
-
-  private static Quad[] onGround =
-      {new Quad(new Vector3(.75, 0, .4375), new Vector3(.25, 0, .4375),
-          new Vector3(.75, 1, .4375), new Vector4(.75, .25, 0, 1)),
-
-          new Quad(new Vector3(.25, 0, .5625), new Vector3(.75, 0, .5625),
-              new Vector3(.25, 1, .5625), new Vector4(.25, .75, 0, 1)),
-
-          new Quad(new Vector3(.4375, 0, .25), new Vector3(.4375, 0, .75),
-              new Vector3(.4375, 1, .25), new Vector4(.25, .75, 0, 1)),
-
-          new Quad(new Vector3(.5625, 0, .75), new Vector3(.5625, 0, .25),
-              new Vector3(.5625, 1, .75), new Vector4(.75, .25, 0, 1)),
-
-          // top
-          new Quad(new Vector3(.4375, .625, .5625), new Vector3(.5625, .625, .5625),
-              new Vector3(.4375, .625, .4375), new Vector4(.4375, .5625, .5, .625)),};
-
-  private static Quad[][] rotQuads = new Quad[6][];
-  private static UVTriangle[][] rotTriangles = new UVTriangle[6][];
+  private static final Quad[][] rotatedQuadsWall = new Quad[6][];
 
   static {
-    rotQuads[0] = new Quad[0];
-    rotTriangles[0] = new UVTriangle[0];
-
-    // pointing west
-    rotQuads[2] = quads;
-    rotTriangles[2] = uvtriangles;
-
-    // pointing north
-    rotQuads[4] = Model.rotateY(rotQuads[2]);
-    rotTriangles[4] = Model.rotateY(rotTriangles[2]);
-
-    // pointing east
-    rotQuads[1] = Model.rotateY(rotQuads[4]);
-    rotTriangles[1] = Model.rotateY(rotTriangles[4]);
-
-    // pointing south
-    rotQuads[3] = Model.rotateY(rotQuads[1]);
-    rotTriangles[3] = Model.rotateY(rotTriangles[1]);
-
-    // on ground
-    rotQuads[5] = onGround;
-    rotTriangles[5] = new UVTriangle[0];
-
+    rotatedQuadsWall[1] = quadsWall; // east
+    rotatedQuadsWall[3] = Model.rotateY(rotatedQuadsWall[1]); // south
+    rotatedQuadsWall[2] = Model.rotateY(rotatedQuadsWall[3]); // west
+    rotatedQuadsWall[4] = Model.rotateY(rotatedQuadsWall[2]); // north
   }
 
   public static boolean intersect(Ray ray, Texture texture) {
@@ -118,40 +119,24 @@ public class TorchModel {
   public static boolean intersect(Ray ray, Texture texture, int rot) {
     boolean hit = false;
     ray.t = Double.POSITIVE_INFINITY;
-    float[] color = null;
-    for (Quad quad : rotQuads[rot]) {
+    float[] color;
+    Quad[] quads = rot < 5 ? rotatedQuadsWall[rot] : quadsGround;
+    for (Quad quad : quads) {
       if (quad.intersect(ray)) {
-        float[] c = texture.getColor(ray.u, ray.v);
-        if (c[3] > Ray.EPSILON) {
-          color = c;
+        color = texture.getColor(ray.u, ray.v);
+        if (color[3] > Ray.EPSILON) {
+          ray.color.set(color);
           ray.n.set(quad.n);
           ray.t = ray.tNext;
           hit = true;
         }
       }
     }
-    for (UVTriangle triangle : rotTriangles[rot]) {
-      if (triangle.intersect(ray)) {
-        float[] c = texture.getColor(ray.u, ray.v);
-        if (c[3] > Ray.EPSILON) {
-          color = c;
-          ray.n.set(triangle.n);
-          ray.t = ray.tNext;
-          hit = true;
-        }
-      }
-    }
+
     if (hit) {
-      double px = ray.o.x - QuickMath.floor(ray.o.x + ray.d.x * Ray.OFFSET) + ray.d.x * ray.tNext;
-      double py = ray.o.y - QuickMath.floor(ray.o.y + ray.d.y * Ray.OFFSET) + ray.d.y * ray.tNext;
-      double pz = ray.o.z - QuickMath.floor(ray.o.z + ray.d.z * Ray.OFFSET) + ray.d.z * ray.tNext;
-      if (px >= 0 && px <= 1 && py >= 0 && py <= 1 && pz >= 0 && pz <= 1) {
-        ray.color.set(color);
-        ray.distance += ray.t;
-        ray.o.scaleAdd(ray.t, ray.d);
-        return true;
-      }
+      ray.distance += ray.t;
+      ray.o.scaleAdd(ray.t, ray.d);
     }
-    return false;
+    return hit;
   }
 }

chunky/src/java/se/llbit/chunky/model/Model.java
@@ -21,7 +21,6 @@ import java.util.Collections;
 import java.util.List;
 import se.llbit.math.Quad;
 import se.llbit.math.Transform;
-import se.llbit.math.UVTriangle;
 import se.llbit.math.Vector3;
 
 /**
@@ -120,18 +119,6 @@ public class Model {
     Quad[] rot = new Quad[src.length];
     for (int i = 0; i < src.length; ++i) {
       rot[i] = src[i].transform(Transform.NONE.rotateNegY());
-    }
-    return rot;
-  }
-
-  /**
-   * @param src source quads
-   * @return UVTriangles rotated about the Y axis
-   */
-  public static UVTriangle[] rotateY(UVTriangle[] src) {
-    UVTriangle[] rot = new UVTriangle[src.length];
-    for (int i = 0; i < src.length; ++i) {
-      rot[i] = src[i].getYRotated();
     }
     return rot;
   }

chunky/src/java/se/llbit/math/UVTriangle.java
@@ -1,155 +1,0 @@
-/* Copyright (c) 2013 Jesper Öqvist <jesper@llbit.se>
- *
- * This file is part of Chunky.
- *
- * Chunky is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Chunky is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with Chunky.  If not, see <http://www.gnu.org/licenses/>.
- */
-package se.llbit.math;
-
-/**
- * UV-mapped triangle
- *
- * @author Jesper Öqvist <jesper@llbit.se>
- */
-public class UVTriangle {
-
-  /**
-   * Normal vector - normal to triangle vertices
-   * in counterclockwise order
-   */
-  public final Vector3 n;
-
-  private final Vector3 a;
-  private final Vector3 b;
-  private final Vector3 c;
-
-  private final Vector3 b_a;
-  private final Vector3 c_a;
-  private final Vector3 c_b;
-  private final Vector3 a_c;
-
-  private final Vector2 sa;
-  private final Vector2 sb;
-  private final Vector2 sc;
-
-  private final double d;
-  private final double rn;
-
-  /**
-   * Construct a new triangle.
-   */
-  public UVTriangle(Vector3 v0, Vector3 v1, Vector3 v2, Vector2 s0,
-      Vector2 s1, Vector2 s2) {
-
-    a = new Vector3(v0);
-    b = new Vector3(v1);
-    c = new Vector3(v2);
-
-    this.sa = new Vector2(s0);
-    this.sb = new Vector2(s1);
-    this.sc = new Vector2(s2);
-
-    b_a = new Vector3();
-    c_a = new Vector3();
-    c_b = new Vector3();
-    a_c = new Vector3();
-
-    b_a.sub(b, a);
-    c_a.sub(c, a);
-    c_b.sub(c, b);
-    a_c.sub(a, c);
-
-    n = new Vector3();
-
-    n.cross(b_a, c_a);
-    rn = 1.0 / n.length();
-    n.normalize();
-
-    d = -n.dot(a);
-  }
-
-  /**
-   * Find intersection between the ray and this triangle.
-   *
-   * @return <code>true</code> if the ray intersects the triangle
-   */
-  public boolean intersect(Ray ray) {
-    double px = ray.o.x - QuickMath.floor(ray.o.x + ray.d.x * Ray.OFFSET);
-    double py = ray.o.y - QuickMath.floor(ray.o.y + ray.d.y * Ray.OFFSET);
-    double pz = ray.o.z - QuickMath.floor(ray.o.z + ray.d.z * Ray.OFFSET);
-
-    // Test that the ray is heading toward the plane.
-    double denom = ray.d.dot(n);
-    if (denom < -Ray.EPSILON) {
-
-      // Test for intersection with the plane at origin.
-      double t = -(px * n.x + py * n.y + pz * n.z + d) / denom;
-      if (t > -Ray.EPSILON && t < ray.t) {
-
-        // Calculate plane intersection point.
-        px = px + ray.d.x * t;
-        py = py + ray.d.y * t;
-        pz = pz + ray.d.z * t;
-
-        // Calculate barycentric coordinates.
-        double nax = c_b.y * (pz - b.z) - c_b.z * (py - b.y);
-        double nay = c_b.z * (px - b.x) - c_b.x * (pz - b.z);
-        double naz = c_b.x * (py - b.y) - c_b.y * (px - b.x);
-
-        double nbx = a_c.y * (pz - c.z) - a_c.z * (py - c.y);
-        double nby = a_c.z * (px - c.x) - a_c.x * (pz - c.z);
-        double nbz = a_c.x * (py - c.y) - a_c.y * (px - c.x);
-
-        double ncx = b_a.y * (pz - a.z) - b_a.z * (py - a.y);
-        double ncy = b_a.z * (px - a.x) - b_a.x * (pz - a.z);
-        double ncz = b_a.x * (py - a.y) - b_a.y * (px - a.x);
-
-        // alpha, beta, gamma are the barycentric coordinates
-        double alpha = (n.x * nax + n.y * nay + n.z * naz) * rn;
-        double beta = (n.x * nbx + n.y * nby + n.z * nbz) * rn;
-        double gamma = (n.x * ncx + n.y * ncy + n.z * ncz) * rn;
-
-        if (alpha >= 0 && beta >= 0 && gamma >= 0) {
-
-          ray.tNext = t;
-          ray.u = alpha * sa.x + beta * sb.x + gamma * sc.x;
-          ray.v = alpha * sa.y + beta * sb.y + gamma * sc.y;
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  /**
-   * @return Rotated copy of this triangle
-   */
-  public UVTriangle getYRotated() {
-    Transform t = Transform.NONE.rotateY();
-    Vector3 ar = new Vector3(a);
-    ar.add(-0.5, -0.5, -0.5);
-    t.apply(ar);
-    ar.add(0.5, 0.5, 0.5);
-    Vector3 br = new Vector3(b);
-    br.add(-0.5, -0.5, -0.5);
-    t.apply(br);
-    br.add(0.5, 0.5, 0.5);
-    Vector3 cr = new Vector3(c);
-    cr.add(-0.5, -0.5, -0.5);
-    t.apply(cr);
-    cr.add(0.5, 0.5, 0.5);
-    return new UVTriangle(ar, br, cr, sa, sb, sc);
-  }
-
-}

commit eca6174d707c8ea4b5798bfe508e0f6c854e5157

Clip the torch model to the block box. (#878)
Fixes a regression introduced by #877

chunky/src/java/se/llbit/chunky/model/TorchModel.java
@@ -119,24 +119,39 @@ public class TorchModel {
   public static boolean intersect(Ray ray, Texture texture, int rot) {
     boolean hit = false;
     ray.t = Double.POSITIVE_INFINITY;
-    float[] color;
+    float[] color = null;
     Quad[] quads = rot < 5 ? rotatedQuadsWall[rot] : quadsGround;
     for (Quad quad : quads) {
       if (quad.intersect(ray)) {
-        color = texture.getColor(ray.u, ray.v);
-        if (color[3] > Ray.EPSILON) {
-          ray.color.set(color);
+        float[] c = texture.getColor(ray.u, ray.v);
+        if (c[3] > Ray.EPSILON) {
+          color = c;
           ray.n.set(quad.n);
           ray.t = ray.tNext;
           hit = true;
         }
       }
     }
-
     if (hit) {
-      ray.distance += ray.t;
-      ray.o.scaleAdd(ray.t, ray.d);
+      if (rot < 5) {
+        // the wall torch model goes out of the 1x1x1 block range so we need to check if the hit
+        // is actually inside of the block and ignore it if it doesn't
+        double px = ray.o.x - Math.floor(ray.o.x + ray.d.x * Ray.OFFSET) + ray.d.x * ray.tNext;
+        double py = ray.o.y - Math.floor(ray.o.y + ray.d.y * Ray.OFFSET) + ray.d.y * ray.tNext;
+        double pz = ray.o.z - Math.floor(ray.o.z + ray.d.z * Ray.OFFSET) + ray.d.z * ray.tNext;
+        if (px >= 0 && px <= 1 && py >= 0 && py <= 1 && pz >= 0 && pz <= 1) {
+          ray.color.set(color);
+          ray.distance += ray.t;
+          ray.o.scaleAdd(ray.t, ray.d);
+          return true;
+        }
+      } else {
+        ray.color.set(color);
+        ray.distance += ray.t;
+        ray.o.scaleAdd(ray.t, ray.d);
+        return true;
+      }
     }
-    return hit;
+    return false;
   }
 }