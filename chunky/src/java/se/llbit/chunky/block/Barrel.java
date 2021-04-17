8c4f4dbdc786e226309577676f8d8bdbe893236e
Maik Marschner <an@maik.tk>
2021-04-18 00:00:30 +0200

    Use OrientedTexturedBlock for the barrel.

--- chunky/src/java/se/llbit/chunky/block/Barrel.java
+++ chunky/src/java/se/llbit/chunky/block/Barrel.java
@@ -1,17 +1,15 @@
 package se.llbit.chunky.block;
 
-import se.llbit.chunky.model.BarrelModel;
 import se.llbit.chunky.resources.Texture;
 
-public class Barrel extends AbstractModelBlock {
+public class Barrel extends OrientedTexturedBlock {
 
   private final String description;
 
-  public Barrel(String facing, String open) {
-    super("barrel", Texture.barrelSide);
+  public Barrel(String facing, boolean open) {
+    super("barrel", facing, Texture.barrelSide, open ? Texture.barrelOpen : Texture.barrelTop,
+        Texture.barrelBottom);
     this.description = "facing=" + facing + ", open=" + open;
-    this.model = new BarrelModel(facing, open);
-    opaque = true;
   }
 
   @Override
