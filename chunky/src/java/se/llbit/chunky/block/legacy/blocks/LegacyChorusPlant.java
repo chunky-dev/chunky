ca1bf40df310ba0e9d40804e950c1161c008f8d4
Maik Marschner <an@maik.tk>
2021-05-11 23:26:25 +0200

    Fix chorus plant connections.

--- chunky/src/java/se/llbit/chunky/block/legacy/blocks/LegacyChorusPlant.java
+++ chunky/src/java/se/llbit/chunky/block/legacy/blocks/LegacyChorusPlant.java
@@ -18,7 +18,7 @@ public class LegacyChorusPlant extends UnfinalizedLegacyBlock {
     CompoundTag newTag = LegacyBlocks.createTag(this.name);
 
     LegacyBlocks.boolTag(newTag, "up", isConnect(state.getMaterial(0, 1, 0)));
-    LegacyBlocks.boolTag(newTag, "down", isConnect(state.getMaterial(0, -1, 0)));
+    LegacyBlocks.boolTag(newTag, "down", isConnectDown(state.getMaterial(0, -1, 0)));
     LegacyBlocks.boolTag(newTag, "east", isConnect(state.getMaterial(1, 0, 0)));
     LegacyBlocks.boolTag(newTag, "west", isConnect(state.getMaterial(-1, 0, 0)));
     LegacyBlocks.boolTag(newTag, "north", isConnect(state.getMaterial(0, 0, -1)));
@@ -29,6 +29,11 @@ public class LegacyChorusPlant extends UnfinalizedLegacyBlock {
 
   private static boolean isConnect(Material block) {
     String name = LegacyBlockUtils.getName(block);
-    return name.equals("chorus_plant") || name.equals("end_stone");
+    return name.equals("chorus_plant") || name.equals("chorus_flower");
+  }
+
+  private static boolean isConnectDown(Material block) {
+    String name = LegacyBlockUtils.getName(block);
+    return name.equals("chorus_plant") || name.equals("chorus_flower") || name.equals("end_stone");
   }
 }
