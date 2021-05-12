69f26c067f5359cfcb7e1e981cf0f96c00235ced
Maik Marschner <an@maik.tk>
2021-05-13 01:33:02 +0200

    Use 1.12 connection logic for iron bars.

--- chunky/src/java/se/llbit/chunky/block/legacy/blocks/LegacyIronBars.java
+++ chunky/src/java/se/llbit/chunky/block/legacy/blocks/LegacyIronBars.java
@@ -15,10 +15,10 @@ public class LegacyIronBars extends UnfinalizedLegacyBlock {
 
   @Override
   public void finalizeBlock(FinalizationState state) {
-    boolean north = isIronBarConnector(state.getMaterial(0, 0, -1));
-    boolean south = isIronBarConnector(state.getMaterial(0, 0, 1));
-    boolean east = isIronBarConnector(state.getMaterial(1, 0, 0));
-    boolean west = isIronBarConnector(state.getMaterial(-1, 0, 0));
+    boolean north = isIronBarConnector(state.getMaterial(0, 0, -1), "north");
+    boolean south = isIronBarConnector(state.getMaterial(0, 0, 1), "south");
+    boolean east = isIronBarConnector(state.getMaterial(1, 0, 0), "east");
+    boolean west = isIronBarConnector(state.getMaterial(-1, 0, 0), "west");
 
     CompoundTag newTag = LegacyBlocks.createTag("iron_bars");
     LegacyBlocks.boolTag(newTag, "east", east);
@@ -29,7 +29,31 @@ public class LegacyIronBars extends UnfinalizedLegacyBlock {
     state.replaceCurrentBlock(newTag);
   }
 
-  private static boolean isIronBarConnector(Material block) {
-    return block.solid || LegacyBlockUtils.getName(block).equals("iron_bars");
+  private static boolean isIronBarConnector(Material block, String direction) {
+    String name = LegacyBlockUtils.getName(block);
+    if (name.equals("cobblestone_wall") || name.equals("mossy_cobblestone_wall") || name
+        .endsWith("_fence") || name
+        .endsWith("_leaves")) { // in 1.9 iron bars do connect to leaves
+      return false;
+    }
+
+    // 1.12 (?) stairs connection logic (only connect to the high side)
+    String adjacentStairsFacing = LegacyBlockUtils.getStairsFacing(block);
+    if (adjacentStairsFacing != null) {
+      switch (direction) {
+        case "north":
+          return adjacentStairsFacing.equals("south");
+        case "south":
+          return adjacentStairsFacing.equals("north");
+        case "east":
+          return adjacentStairsFacing.equals("west");
+        case "west":
+          return adjacentStairsFacing.equals("east");
+        default:
+          return false;
+      }
+    }
+
+    return block.solid || name.equals("iron_bars");
   }
 }
