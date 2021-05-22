30f3fca7ef70917291da1bdcb08ca72fc01079c1
Thomas Ferrand <a.tom.hic3@gmail.com>
2021-05-22 19:44:44 +0200

    Don't use the TextureMaterial cache for Texture that are not shared

--- chunky/src/java/se/llbit/chunky/entity/SignEntity.java
+++ chunky/src/java/se/llbit/chunky/entity/SignEntity.java
@@ -528,17 +528,21 @@ public class SignEntity extends Entity {
     Collection<Primitive> primitives = new LinkedList<>();
     Transform transform = Transform.NONE
       .translate(position.x + offset.x, position.y + offset.y, position.z + offset.z);
+    // Front and back textures are unique so it is useless to use the cache for them
+    // it will just take space in the cache for nothing
+    // The other texture is shared so retrieving the material from the cache is a gain
+    TextureMaterial otherMaterial = TextureMaterial.getForTexture(texture);
     for (int i = 0; i < sides.length; ++i) {
       Quad quad = rot[angle][i];
-      Texture tex = texture;
       if (i == 0 && frontTexture != null) {
-        tex = frontTexture;
         quad = frontFaceWithText[angle];
+        quad.addTriangles(primitives, new TextureMaterial(frontTexture), transform);
       } else if (i == 1 && backTexture != null) {
-        tex = backTexture;
         quad = backFaceWithText[angle];
+        quad.addTriangles(primitives, new TextureMaterial(backTexture), transform);
+      } else {
+        quad.addTriangles(primitives, otherMaterial, transform);
       }
-      quad.addTriangles(primitives, TextureMaterial.getForTexture(tex), transform);
     }
     return primitives;
   }
