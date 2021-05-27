3b615eabdeb8683fb37954cf3269b5f1fe9e0bed
Thomas Ferrand <a.tom.hic3@gmail.com>
2021-05-27 22:55:41 +0200

    Don't cache the TextureMaterial for the Texture that has just been created

--- chunky/src/java/se/llbit/chunky/entity/StandingBanner.java
+++ chunky/src/java/se/llbit/chunky/entity/StandingBanner.java
@@ -464,6 +464,6 @@ public class StandingBanner extends Entity {
     }
     Texture texture = new Texture(tinted);
     TextureCache.put(design, texture);
-    return TextureMaterial.getForTexture(texture);
+    return new TextureMaterial(texture);
   }
 }
