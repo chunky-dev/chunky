a5ff5a7ee26f03074b0670034d749e9b5af1b699
Alex <alexhliu02@gmail.com>
2021-06-07 21:13:01 -0700

    Implement BrewingStand.

--- chunky/src/java/se/llbit/chunky/model/BrewingStandModel.java
+++ chunky/src/java/se/llbit/chunky/model/BrewingStandModel.java
@@ -17,28 +17,161 @@
 package se.llbit.chunky.model;
 
 import se.llbit.chunky.resources.Texture;
-import se.llbit.math.AABB;
 import se.llbit.math.DoubleSidedQuad;
 import se.llbit.math.Quad;
-import se.llbit.math.QuickMath;
-import se.llbit.math.Ray;
 import se.llbit.math.Vector3;
 import se.llbit.math.Vector4;
 
-public class BrewingStandModel {
-  private static final AABB[] boxes = {
-      new AABB(7 / 16., 9 / 16., 0, 14 / 16., 7 / 16., 9 / 16.),
-      new AABB(9 / 16., 15 / 16., 0, 2 / 16., 5 / 16., 11 / 16.),
-      new AABB(2 / 16., 8 / 16., 0, 2 / 16., 9 / 16., 15 / 16.),
-      new AABB(2 / 16., 8 / 16., 0, 2 / 16., 1 / 16., 7 / 16.),
+public class BrewingStandModel extends QuadModel {
+  //region quads
+  private static final Quad[] baseQuads = new Quad[] {
+      new Quad(
+          new Vector3(7 / 16.0, 14 / 16.0, 9 / 16.0),
+          new Vector3(9 / 16.0, 14 / 16.0, 9 / 16.0),
+          new Vector3(7 / 16.0, 14 / 16.0, 7 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 7 / 16.0, 9 / 16.0)
+      ),
+      new Quad(
+          new Vector3(7 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector3(7 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(7 / 16.0, 9 / 16.0, 7 / 16.0, 9 / 16.0)
+      ),
+      new Quad(
+          new Vector3(7 / 16.0, 14 / 16.0, 9 / 16.0),
+          new Vector3(7 / 16.0, 14 / 16.0, 7 / 16.0),
+          new Vector3(7 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(9 / 16.0, 7 / 16.0, 14 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 14 / 16.0, 7 / 16.0),
+          new Vector3(9 / 16.0, 14 / 16.0, 9 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector4(9 / 16.0, 7 / 16.0, 14 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(7 / 16.0, 14 / 16.0, 7 / 16.0),
+          new Vector3(9 / 16.0, 14 / 16.0, 7 / 16.0),
+          new Vector3(7 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector4(9 / 16.0, 7 / 16.0, 14 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 14 / 16.0, 9 / 16.0),
+          new Vector3(7 / 16.0, 14 / 16.0, 9 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(9 / 16.0, 7 / 16.0, 14 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 2 / 16.0, 11 / 16.0),
+          new Vector3(15 / 16.0, 2 / 16.0, 11 / 16.0),
+          new Vector3(9 / 16.0, 2 / 16.0, 5 / 16.0),
+          new Vector4(9 / 16.0, 15 / 16.0, 5 / 16.0, 11 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 0 / 16.0, 5 / 16.0),
+          new Vector3(15 / 16.0, 0 / 16.0, 5 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 11 / 16.0),
+          new Vector4(9 / 16.0, 15 / 16.0, 5 / 16.0, 11 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 2 / 16.0, 11 / 16.0),
+          new Vector3(9 / 16.0, 2 / 16.0, 5 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 11 / 16.0),
+          new Vector4(11 / 16.0, 5 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(15 / 16.0, 2 / 16.0, 5 / 16.0),
+          new Vector3(15 / 16.0, 2 / 16.0, 11 / 16.0),
+          new Vector3(15 / 16.0, 0 / 16.0, 5 / 16.0),
+          new Vector4(11 / 16.0, 5 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(9 / 16.0, 2 / 16.0, 5 / 16.0),
+          new Vector3(15 / 16.0, 2 / 16.0, 5 / 16.0),
+          new Vector3(9 / 16.0, 0 / 16.0, 5 / 16.0),
+          new Vector4(15 / 16.0, 9 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(15 / 16.0, 2 / 16.0, 11 / 16.0),
+          new Vector3(9 / 16.0, 2 / 16.0, 11 / 16.0),
+          new Vector3(15 / 16.0, 0 / 16.0, 11 / 16.0),
+          new Vector4(15 / 16.0, 9 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 2 / 16.0, 7 / 16.0),
+          new Vector3(8 / 16.0, 2 / 16.0, 7 / 16.0),
+          new Vector3(2 / 16.0, 2 / 16.0, 1 / 16.0),
+          new Vector4(2 / 16.0, 8 / 16.0, 9 / 16.0, 15 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 0 / 16.0, 1 / 16.0),
+          new Vector3(8 / 16.0, 0 / 16.0, 1 / 16.0),
+          new Vector3(2 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector4(2 / 16.0, 8 / 16.0, 9 / 16.0, 15 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 2 / 16.0, 7 / 16.0),
+          new Vector3(2 / 16.0, 2 / 16.0, 1 / 16.0),
+          new Vector3(2 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector4(7 / 16.0, 1 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(8 / 16.0, 2 / 16.0, 1 / 16.0),
+          new Vector3(8 / 16.0, 2 / 16.0, 7 / 16.0),
+          new Vector3(8 / 16.0, 0 / 16.0, 1 / 16.0),
+          new Vector4(7 / 16.0, 1 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 2 / 16.0, 1 / 16.0),
+          new Vector3(8 / 16.0, 2 / 16.0, 1 / 16.0),
+          new Vector3(2 / 16.0, 0 / 16.0, 1 / 16.0),
+          new Vector4(8 / 16.0, 2 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(8 / 16.0, 2 / 16.0, 7 / 16.0),
+          new Vector3(2 / 16.0, 2 / 16.0, 7 / 16.0),
+          new Vector3(8 / 16.0, 0 / 16.0, 7 / 16.0),
+          new Vector4(8 / 16.0, 2 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 2 / 16.0, 15 / 16.0),
+          new Vector3(8 / 16.0, 2 / 16.0, 15 / 16.0),
+          new Vector3(2 / 16.0, 2 / 16.0, 9 / 16.0),
+          new Vector4(2 / 16.0, 8 / 16.0, 1 / 16.0, 7 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector3(8 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector3(2 / 16.0, 0 / 16.0, 15 / 16.0),
+          new Vector4(2 / 16.0, 8 / 16.0, 1 / 16.0, 7 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 2 / 16.0, 15 / 16.0),
+          new Vector3(2 / 16.0, 2 / 16.0, 9 / 16.0),
+          new Vector3(2 / 16.0, 0 / 16.0, 15 / 16.0),
+          new Vector4(15 / 16.0, 9 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(8 / 16.0, 2 / 16.0, 9 / 16.0),
+          new Vector3(8 / 16.0, 2 / 16.0, 15 / 16.0),
+          new Vector3(8 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(15 / 16.0, 9 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(2 / 16.0, 2 / 16.0, 9 / 16.0),
+          new Vector3(8 / 16.0, 2 / 16.0, 9 / 16.0),
+          new Vector3(2 / 16.0, 0 / 16.0, 9 / 16.0),
+          new Vector4(8 / 16.0, 2 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
+      new Quad(
+          new Vector3(8 / 16.0, 2 / 16.0, 15 / 16.0),
+          new Vector3(2 / 16.0, 2 / 16.0, 15 / 16.0),
+          new Vector3(8 / 16.0, 0 / 16.0, 15 / 16.0),
+          new Vector4(8 / 16.0, 2 / 16.0, 2 / 16.0, 0 / 16.0)
+      ),
   };
 
-  private static final Texture[] tex = {
-      Texture.brewingStandSide, Texture.brewingStandBase,
-      Texture.brewingStandBase, Texture.brewingStandBase,
-  };
-
-  private static final Quad[] quads = new Quad[] {
+  private static final DoubleSidedQuad[] noBottleQuads = {
       // east
       new DoubleSidedQuad(new Vector3(9 / 16., 0, .5), new Vector3(1, 0, .5),
           new Vector3(.5, 1, .5), new Vector4(9 / 16., 1, 0, 1)),
@@ -52,40 +185,46 @@ public class BrewingStandModel {
           new Vector3(.46, 1, 7 / 16.), new Vector4(9 / 16., 1, 0, 1)),
   };
 
-  public static boolean intersect(Ray ray) {
-    int data = ray.getBlockData();
-    return intersect(ray, data);
+  private static final DoubleSidedQuad[] bottleQuads = {
+      // east
+      new DoubleSidedQuad(new Vector3(9 / 16., 0, .5), new Vector3(1, 0, .5),
+          new Vector3(.50, 1, 8 / 16.), new Vector4(7 / 16., 0, 0, 1)),
+
+      // southwest 210
+      new DoubleSidedQuad(new Vector3(.46, 0, 9 / 16.), new Vector3(.25, 0, .933),
+          new Vector3(.46, 1, 9 / 16.), new Vector4(7 / 16., 0., 0, 1)),
+
+      // northwest 330
+      new DoubleSidedQuad(new Vector3(.46, 0, 7 / 16.), new Vector3(.25, 0, .067),
+          new Vector3(.46, 1, 7 / 16.), new Vector4(7 / 16., 0, 0, 1)),
+  };
+  //endregion
+
+  private static final Texture base = Texture.brewingStandBase;
+  private static final Texture stand = Texture.brewingStandSide;
+  private static final Texture[] tex = new Texture[] {
+      stand, stand, stand, stand, stand, stand, base, base, base, base, base, base, base, base, base, base, base,
+      base, base, base, base, base, base, base, stand, stand, stand
+  };
+
+  private final Quad[] quads;
+
+  public BrewingStandModel(boolean bottle0, boolean bottle1, boolean bottle2) {
+    int len = baseQuads.length;
+    quads = new Quad[len + 3];
+    System.arraycopy(baseQuads, 0, quads, 0, len);
+    quads[len + 0] = bottle0 ? bottleQuads[0] : noBottleQuads[0];
+    quads[len + 1] = bottle1 ? bottleQuads[1] : noBottleQuads[1];
+    quads[len + 2] = bottle2 ? bottleQuads[2] : noBottleQuads[2];
   }
 
-  public static boolean intersect(Ray ray, int data) {
-    boolean hit = false;
-    ray.t = Double.POSITIVE_INFINITY;
-    for (int i = 0; i < boxes.length; ++i) {
-      if (boxes[i].intersect(ray)) {
-        ray.t = ray.tNext;
-        tex[i].getColor(ray);
-        ray.color.w = 1;
-        hit = true;
-      }
-    }
-    for (int i = 0; i < quads.length; ++i) {
-      Quad quad = quads[i];
-      if (quad.intersect(ray)) {
-        int bottle = (data >> i) & 1;
-        float[] color = Texture.brewingStandSide.getColor(bottle + (1 - 2 * bottle) * ray.u, ray.v);
-        if (color[3] > Ray.EPSILON) {
-          ray.color.set(color);
-          ray.t = ray.tNext;
-          ray.n.set(quad.n);
-          ray.n.scale(QuickMath.signum(-ray.d.dot(quad.n)));
-          hit = true;
-        }
-      }
-    }
-    if (hit) {
-      ray.distance += ray.t;
-      ray.o.scaleAdd(ray.t, ray.d);
-    }
-    return hit;
+  @Override
+  public Quad[] getQuads() {
+    return quads;
+  }
+
+  @Override
+  public Texture[] getTextures() {
+    return tex;
   }
 }
