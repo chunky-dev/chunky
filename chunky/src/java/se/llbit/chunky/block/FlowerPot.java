ef2bf8956870d9616fa0ff3670988ea1b2b3c7cc
Alex <alexhliu02@gmail.com>
2021-06-07 23:09:01 -0700

    FlowerPot model.

--- chunky/src/java/se/llbit/chunky/block/FlowerPot.java
+++ chunky/src/java/se/llbit/chunky/block/FlowerPot.java
@@ -1,21 +1,11 @@
 package se.llbit.chunky.block;
 
 import se.llbit.chunky.model.FlowerPotModel;
-import se.llbit.chunky.model.PistonModel;
-import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
-import se.llbit.math.Ray;
 
-public class FlowerPot extends MinecraftBlockTranslucent {
-  private final FlowerPotModel.Kind kind;
-
+public class FlowerPot extends AbstractModelBlock {
   public FlowerPot(String name, FlowerPotModel.Kind kind) {
     super(name, Texture.flowerPot);
-    this.kind = kind;
-    localIntersect = true;
-  }
-
-  @Override public boolean intersect(Ray ray, Scene scene) {
-    return FlowerPotModel.intersect(ray, scene, kind);
+    this.model = new FlowerPotModel(kind);
   }
 }
