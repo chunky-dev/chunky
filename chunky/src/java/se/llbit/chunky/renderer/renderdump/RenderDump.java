273394ba10c730d68d57d4e28309ea9e44fdff72
LasaBir <StanleyMines@users.noreply.github.com>
2021-08-03 14:40:49 -0700

    Use buffered streams for dump loading and saving (#1012)
    * Switch To Using FastBuffered IO Streams.
    
    * FastBufferedOutputStream causes NPE that Java's BufferedOutputStream does not.
    
    Crash was on buffer flush, saying underlying output stream no longer existed. Works perfectly fine with this one. No features exclusive to FastBufferedInputStream or FastBufferedOutputStream are used anywhere.
    
    * Switched to buffered input/output streams from Fast
    
    as requested by @leMaik.
    
    No crashes, as far as I can tell.

--- chunky/src/java/se/llbit/chunky/renderer/renderdump/RenderDump.java
+++ chunky/src/java/se/llbit/chunky/renderer/renderdump/RenderDump.java
@@ -16,11 +16,11 @@
  */
 package se.llbit.chunky.renderer.renderdump;
 
-import it.unimi.dsi.fastutil.io.FastBufferedInputStream;
-import it.unimi.dsi.fastutil.io.FastBufferedOutputStream;
 import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.util.TaskTracker;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -60,7 +60,7 @@ public class RenderDump {
       throws IOException, IllegalStateException {
     int magicNumberLength = DUMP_FORMAT_MAGIC_NUMBER.length;
 
-    PushbackInputStream pushbackInputStream = new PushbackInputStream(inputStream, magicNumberLength);
+    PushbackInputStream pushbackInputStream = new PushbackInputStream(new BufferedInputStream(inputStream), magicNumberLength);
     byte[] magicNumber = new byte[magicNumberLength];
 
     // If the file starts with the magic number, it is the new format containing a version number
@@ -88,7 +88,7 @@ public class RenderDump {
       throws IOException, IllegalStateException {
     int magicNumberLength = DUMP_FORMAT_MAGIC_NUMBER.length;
 
-    PushbackInputStream pushbackInputStream = new PushbackInputStream(new FastBufferedInputStream(inputStream), magicNumberLength);
+    PushbackInputStream pushbackInputStream = new PushbackInputStream(new BufferedInputStream(inputStream), magicNumberLength);
     byte[] magicNumber = new byte[magicNumberLength];
 
     // If the file starts with the magic number, it is the new format containing a version number
@@ -108,7 +108,7 @@ public class RenderDump {
 
   public static void save(OutputStream outputStream, Scene scene, TaskTracker taskTracker) throws IOException {
     outputStream.write(DUMP_FORMAT_MAGIC_NUMBER);
-    DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+    DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(outputStream));
     DumpFormat format = getDumpFormatForVersion(CURRENT_DUMP_VERSION);
     dataOutputStream.writeInt(CURRENT_DUMP_VERSION);
     format.save(dataOutputStream, scene, taskTracker);
