9e6f23df17d8ff747e81c8f149de8113987f08f1
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2021-10-01 22:46:38 +0200

    rename Camera::worldWidth to be more descriptive

--- chunky/src/java/se/llbit/chunky/renderer/scene/Camera.java
+++ chunky/src/java/se/llbit/chunky/renderer/scene/Camera.java
@@ -34,7 +34,6 @@ import se.llbit.chunky.renderer.projection.ShiftProjector;
 import se.llbit.chunky.renderer.projection.SphericalApertureProjector;
 import se.llbit.chunky.renderer.projection.StereographicProjector;
 import se.llbit.chunky.world.Chunk;
-import se.llbit.chunky.entity.PlayerEntity;
 import se.llbit.json.JsonObject;
 import se.llbit.log.Log;
 import se.llbit.math.Matrix3;
@@ -147,7 +146,7 @@ public class Camera implements JsonSerializable {
   /**
    * Maximum diagonal width of the world. Recalculated when world is loaded.
    */
-  private double worldWidth = 100;
+  private double worldDiagonalSize = 100;
 
   private double subjectDistance = 2;
 
@@ -177,7 +176,7 @@ public class Camera implements JsonSerializable {
     projectionMode = other.projectionMode;
     fov = other.fov;
     subjectDistance = other.subjectDistance;
-    worldWidth = other.worldWidth;
+    worldDiagonalSize = other.worldDiagonalSize;
     this.shiftX = other.shiftX;
     this.shiftY = other.shiftY;
     initProjector();
@@ -212,8 +211,12 @@ public class Camera implements JsonSerializable {
         return applyShift(applyDoF(new PinholeProjector(fov), subjectDistance));
       case PARALLEL:
         return applyShift(applyDoF(
-            new ForwardDisplacementProjector(new ParallelProjector(worldWidth, fov), -worldWidth),
-            subjectDistance + worldWidth));
+            new ForwardDisplacementProjector(
+              new ParallelProjector(worldDiagonalSize, fov),
+              -worldDiagonalSize
+            ),
+            subjectDistance + worldDiagonalSize
+        ));
       case FISHEYE:
         return applySphericalDoF(new FisheyeProjector(fov));
       case PANORAMIC_SLOT:
@@ -575,7 +578,7 @@ public class Camera implements JsonSerializable {
    * @param size World size
    */
   public void setWorldSize(double size) {
-    worldWidth = 2 * Math.sqrt(2 * size * size + Chunk.Y_MAX * Chunk.Y_MAX);
+    worldDiagonalSize = 2 * Math.sqrt(2 * size * size + Chunk.Y_MAX * Chunk.Y_MAX);
     if (projectionMode == ProjectionMode.PARALLEL) {
       initProjector();
     }
