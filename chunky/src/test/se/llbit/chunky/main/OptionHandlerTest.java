99c8943762e30af11dbb6d043d2c3b00ee206b7f
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2021-10-05 22:00:42 +0200

    add numeric option arguments to command line argument parser

--- chunky/src/test/se/llbit/chunky/main/OptionHandlerTest.java
+++ chunky/src/test/se/llbit/chunky/main/OptionHandlerTest.java
@@ -67,6 +67,10 @@ public class OptionHandlerTest {
     OptionHandler handler2 = new OptionHandler("-bart", new Range(1), this::expectBart);
     List<String> extraArgs2 = handler2.handle(Arrays.asList("bart", "foo"));
     assertThat(extraArgs2).containsExactly("foo");
+
+    OptionHandler handler = new OptionHandler("-bart", new Range(1), new int[]{0}, this::expectBart);
+    List<String> extraArgs = handler.handle(Arrays.asList("bart", "-42"));
+    assertThat(extraArgs).containsExactly("-42");
   }
 
   /** Test an option that accepts 0 to 2 arguments. */
@@ -92,6 +96,42 @@ public class OptionHandlerTest {
     assertThat(extraArgs5).containsExactly("-foo", "bar");
   }
 
+  @Test public void testArgRangeWithNumeric() throws InvalidCommandLineArgumentsException {
+    OptionHandler handler1 = new OptionHandler("-bart", new Range(0, 2), new int[]{0}, this::expectNone);
+    List<String> extraArgs1 = handler1.handle(Collections.emptyList());
+    assertThat(extraArgs1).isEmpty();
+
+    OptionHandler handler2 = new OptionHandler("-bart", new Range(0, 2), new int[]{0}, args -> {
+      assertThat(args).containsExactly("-42", "foo");
+    });
+    List<String> extraArgs2 = handler2.handle(Arrays.asList("-42", "foo"));
+    assertThat(extraArgs2).isEmpty();
+
+    OptionHandler handler3 = new OptionHandler("-bart", new Range(0, 2), new int[]{1}, args -> {
+      assertThat(args).containsExactly("foo", "-42");
+    });
+    List<String> extraArgs3 = handler3.handle(Arrays.asList("foo", "-42"));
+    assertThat(extraArgs3).isEmpty();
+
+    OptionHandler handler4 = new OptionHandler("-bart", new Range(0, 2), new int[]{1}, args -> {
+      assertThat(args).containsExactly("foo");
+    });
+    List<String> extraArgs4 = handler4.handle(Arrays.asList("foo", "-next"));
+    assertThat(extraArgs4).containsExactly("-next");
+
+    OptionHandler handler5 = new OptionHandler("-bart", new Range(0, 2), new int[]{0}, args -> {
+      assertThat(args).containsExactly("-42");
+    });
+    List<String> extraArgs5 = handler5.handle(Arrays.asList("-42", "-next"));
+    assertThat(extraArgs5).containsExactly("-next");
+
+    OptionHandler handler6 = new OptionHandler("-bart", new Range(0, 2), new int[]{0}, args -> {
+      assertThat(args).containsExactly("-42");
+    });
+    List<String> extraArgs6 = handler6.handle(Arrays.asList("-42", "-42"));
+    assertThat(extraArgs6).containsExactly("-42");
+  }
+
   @Test(expected = IllegalArgumentException.class)
   public void testError_NoOptionGiven() throws InvalidCommandLineArgumentsException {
     OptionHandler handler = new OptionHandler("-bart", new Range(1), arguments -> {});
@@ -109,5 +149,11 @@ public class OptionHandlerTest {
   public void testError_ArgumentInsteadOfOptionGiven() throws InvalidCommandLineArgumentsException {
     OptionHandler handler = new OptionHandler("-bart", new Range(1), this::expectBart);
     handler.handle(Collections.singletonList("bort"));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testError_NumberInsteadOfOptionGiven() throws InvalidCommandLineArgumentsException {
+    OptionHandler handler = new OptionHandler("-bart", new Range(1), this::expectBart);
+    handler.handle(Collections.singletonList("-42"));
   }
 }
