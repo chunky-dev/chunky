6dd807e76b88b31c9abc91b69bf2536903ce3c20
Stéphane Wouters <kyxalia@gmail.com>
2021-12-27 15:17:29 +0100

    Add -reload-chunks option to headless mode (#1102)
    * Add -reload-chunks option to headless mode
    
    * Add -reload-chunks and -f to usage string.
    
    Co-authored-by: Maik Marschner <an@maik.tk>

--- chunky/src/java/se/llbit/chunky/main/CommandLineOptions.java
+++ chunky/src/java/se/llbit/chunky/main/CommandLineOptions.java
@@ -73,6 +73,8 @@ public class CommandLineOptions {
           "  -tile-width <NUM>      use the specified tile width for rendering",
           "  -spp-per-pass <NUM>    use the specified samples per pixel per pass for rendering",
           "  -target <NUM>          override target SPP to be NUM in headless mode",
+          "  -reload-chunks         reload the selected chunks before rendering the scene",
+          "  -f                     render the scene even if loading the scene fails (e.g. ignore missing octree)",
           "  -set <NAME> <VALUE>    set a global configuration option and exit",
           "  -set <NAME> <VALUE> <SCENE>",
           "                         set a configuration option for a scene and exit",
@@ -256,6 +258,10 @@ public class CommandLineOptions {
 
     registerOption("-f", new Range(0), arguments -> {
       options.force = true;
+    });
+
+    registerOption("-reload-chunks", new Range(0), arguments -> {
+      options.reloadChunks = true;
     });
 
     registerOption("-target", new Range(1),
