e837848698afe7b07df97b002d48f020876dc24d
Alex <alexhliu02@gmail.com>
2021-12-30 18:38:01 -0800

    Make EmitterSamplingStrategy implement Registerable. Add descriptions to each strategy.

--- chunky/src/java/se/llbit/chunky/renderer/EmitterSamplingStrategy.java
+++ chunky/src/java/se/llbit/chunky/renderer/EmitterSamplingStrategy.java
@@ -1,5 +1,33 @@
 package se.llbit.chunky.renderer;
 
-public enum EmitterSamplingStrategy {
-  NONE, ONE, ONE_BLOCK, ALL
+import se.llbit.util.Registerable;
+
+public enum EmitterSamplingStrategy implements Registerable {
+  NONE("None", "No emitter sampling."),
+  ONE("One", "Sample a single face."),
+  ONE_BLOCK("One Block", "Sample all the faces on a single emitter block."),
+  ALL("All", "Sample all faces on all emitter blocks.");
+
+  private final String name;
+  private final String description;
+
+  EmitterSamplingStrategy(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
+  }
+
+  @Override
+  public String getId() {
+    return this.name();
+  }
 }
