2a562ebd79734243f25eb32333942beb5a737cc4
Maik Marschner <an@maik.tk>
2022-04-16 02:29:45 +0200

    Refactor special biome colors to be less special.

--- chunky/src/java/se/llbit/chunky/world/biome/Biome.java
+++ chunky/src/java/se/llbit/chunky/world/biome/Biome.java
@@ -22,20 +22,17 @@ import static se.llbit.math.ColorUtil.getRGBAComponentsGammaCorrected;
  * @author Jesper Öqvist <jesper@llbit.se>
  */
 public class Biome {
+  public static final int DEFAULT_WATER_COLOR = 0x3f76e4;
   public final String resourceLocation;
   public final String name;
   public final float temperature;
   public final float rain;
   public final int mapColor;
 
-  /**
-   * Default grass color before loading from resource pack.
-   */
   public int grassColor;
-  /**
-   * Default foliage color before loading from resource pack.
-   */
+  public GrassColorMode grassColorMode;
   public int foliageColor;
+  public FoliageColorMode foliageColorMode;
   public int waterColor;
 
   public float[] grassColorLinear;
@@ -43,21 +40,68 @@ public class Biome {
   public float[] waterColorLinear;
 
   Biome(String resourceLocation, String name, double temperature, double rain, int mapColor, int grassColor, int foliageColor) {
-    this(resourceLocation, name, temperature, rain, mapColor, grassColor, foliageColor, 0x3f76e4);
+    this(resourceLocation, name, temperature, rain, mapColor, grassColor, foliageColor, DEFAULT_WATER_COLOR);
   }
 
   Biome(String resourceLocation, String name, double temperature, double rain, int mapColor, int grassColor, int foliageColor, int waterColor) {
+    this(resourceLocation, name, temperature, rain, mapColor, grassColor, GrassColorMode.DEFAULT, foliageColor, FoliageColorMode.DEFAULT, waterColor);
+  }
+
+  Biome(String resourceLocation, String name, double temperature, double rain, int mapColor, int grassColor,
+        GrassColorMode grassColorMode, int foliageColor, FoliageColorMode foliageColorMode, int waterColor) {
     this.resourceLocation = resourceLocation;
     this.name = name;
     this.temperature = (float) temperature;
     this.rain = (float) rain;
     this.mapColor = 0xFF000000 | mapColor;
     this.grassColor = grassColor;
+    this.grassColorMode = grassColorMode;
     this.foliageColor = foliageColor;
+    this.foliageColorMode = foliageColorMode;
     this.waterColor = waterColor;
 
     this.grassColorLinear = getRGBAComponentsGammaCorrected(grassColor);
     this.foliageColorLinear = getRGBAComponentsGammaCorrected(foliageColor);
     this.waterColorLinear = getRGBAComponentsGammaCorrected(waterColor);
+  }
+
+  public static BiomeBuilder create(String resourceLocation, String name, double temperature, double rain) {
+    return new BiomeBuilder(resourceLocation, name, temperature, rain);
+  }
+
+  enum GrassColorMode {
+    /**
+     * The grass color depends on temperature and humidity.
+     */
+    DEFAULT,
+    /**
+     * The grass color is fixed.
+     */
+    FIXED_COLOR,
+    /**
+     * The grass color depends on temperature and humidity and is also averaged with 0x28340A.
+     */
+    DARK_FOREST,
+    /**
+     * In Java Edition, the grass color would be perlin noise with two colors.
+     * Since we don't support this, the grass color is 0x6A7039 (as in Bedrock Edition).
+     */
+    SWAMP
+  }
+
+  enum FoliageColorMode {
+    /**
+     * The foliage color depends on temperature and humidity.
+     */
+    DEFAULT,
+    /**
+     * The foliage color is fixed.
+     */
+    FIXED_COLOR,
+    /**
+     * In Java Edition, the foliage color would be perlin noise with two colors.
+     * Since we don't support this, the foliage color is 0x6A7039 (as in Bedrock Edition).
+     */
+    SWAMP
   }
 }
