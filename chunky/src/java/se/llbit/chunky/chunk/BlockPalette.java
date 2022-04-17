4fa246e322c104bbaf1b134073a26c51585c302c
Peregrine05 <92183530+Peregrine05@users.noreply.github.com>
2022-04-17 02:44:44 +0200

    Update material defaults
    Water specular now 0.255;
    Torch and wall torch emittance now 5.0;
    Soul torch and Soul wall torch emittance now 3.5;
    Added Redstone wall torch emittance of 1.0;
    Added Enchanting table emittance of 0.6;

--- chunky/src/java/se/llbit/chunky/chunk/BlockPalette.java
+++ chunky/src/java/se/llbit/chunky/chunk/BlockPalette.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019-2021 Chunky contributors
+/* Copyright (c) 2019-2022 Chunky contributors
  *
  * This file is part of Chunky.
  *
@@ -236,7 +236,7 @@ public class BlockPalette {
     materialProperties.put(
         "minecraft:water",
         block -> {
-          block.specular = 0.12f;
+          block.specular = 0.255f;
           block.ior = 1.333f;
           block.refractive = true;
         });
@@ -338,11 +338,14 @@ public class BlockPalette {
     materialProperties.put("minecraft:redstone_torch", block -> {
       block.emittance = 1.0f;
     });
+    materialProperties.put("minecraft:redstone_wall_torch", block -> {
+      block.emittance = 1.0f;
+    });
     materialProperties.put("minecraft:torch", block -> {
-      block.emittance = 50.0f;
+      block.emittance = 5.0f;
     });
     materialProperties.put("minecraft:wall_torch", block -> {
-      block.emittance = 50.0f;
+      block.emittance = 5.0f;
     });
     materialProperties.put("minecraft:fire", block -> {
       block.emittance = 1.0f;
@@ -440,21 +443,24 @@ public class BlockPalette {
       block.emittance = 0.6f;
     });
     materialProperties.put("minecraft:soul_fire_torch", block -> { // MC 20w06a-20w16a
-      block.emittance = 35.0f;
+      block.emittance = 3.5f;
     });
     materialProperties.put("minecraft:soul_torch", block -> { // MC >= 20w17a
-      block.emittance = 35.0f;
+      block.emittance = 3.5f;
     });
     materialProperties.put("minecraft:soul_fire_wall_torch", block -> { // MC 20w06a-20w16a
-      block.emittance = 35.0f;
+      block.emittance = 3.5f;
     });
     materialProperties.put("minecraft:soul_wall_torch", block -> { // MC >= 20w17a
-      block.emittance = 35.0f;
+      block.emittance = 3.5f;
     });
     materialProperties.put("minecraft:soul_fire", block -> {
       block.emittance = 0.6f;
     });
     materialProperties.put("minecraft:crying_obsidian", block -> {
+      block.emittance = 0.6f;
+    });
+    materialProperties.put("minecraft:enchanting_table", block -> {
       block.emittance = 0.6f;
     });
     materialProperties.put("minecraft:respawn_anchor", block -> {
