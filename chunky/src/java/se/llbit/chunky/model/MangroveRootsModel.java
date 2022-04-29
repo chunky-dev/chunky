604551e06dcf3e8f22f62c3e4a6057c38bfd38f9
Peregrine05 <92183530+Peregrine05@users.noreply.github.com>
2022-04-30 00:08:17 +0200

    Update mangrove roots model to match 22w17a (#1249)

--- chunky/src/java/se/llbit/chunky/model/MangroveRootsModel.java
+++ chunky/src/java/se/llbit/chunky/model/MangroveRootsModel.java
@@ -15,27 +15,27 @@ public class MangroveRootsModel extends QuadModel {
 
   private static final Quad[] quads = new Quad[]{
     new Quad(
-      new Vector3(0.8 / 16.0, 16 / 16.0, 8 / 16.0),
-      new Vector3(15.2 / 16.0, 16 / 16.0, 8 / 16.0),
-      new Vector3(0.8 / 16.0, 0 / 16.0, 8 / 16.0),
+      new Vector3(0 / 16.0, 16 / 16.0, 8 / 16.0),
+      new Vector3(16 / 16.0, 16 / 16.0, 8 / 16.0),
+      new Vector3(0 / 16.0, 0 / 16.0, 8 / 16.0),
       new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
     ),
     new Quad(
-      new Vector3(15.2 / 16.0, 16 / 16.0, 8 / 16.0),
-      new Vector3(0.8 / 16.0, 16 / 16.0, 8 / 16.0),
-      new Vector3(15.2 / 16.0, 0 / 16.0, 8 / 16.0),
+      new Vector3(16 / 16.0, 16 / 16.0, 8 / 16.0),
+      new Vector3(0 / 16.0, 16 / 16.0, 8 / 16.0),
+      new Vector3(16 / 16.0, 0 / 16.0, 8 / 16.0),
       new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
     ),
     new Quad(
-      new Vector3(8 / 16.0, 16 / 16.0, 15.2 / 16.0),
-      new Vector3(8 / 16.0, 16 / 16.0, 0.8 / 16.0),
-      new Vector3(8 / 16.0, 0 / 16.0, 15.2 / 16.0),
+      new Vector3(8 / 16.0, 16 / 16.0, 16 / 16.0),
+      new Vector3(8 / 16.0, 16 / 16.0, 0 / 16.0),
+      new Vector3(8 / 16.0, 0 / 16.0, 16 / 16.0),
       new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
     ),
     new Quad(
-      new Vector3(8 / 16.0, 16 / 16.0, 0.8 / 16.0),
-      new Vector3(8 / 16.0, 16 / 16.0, 15.2 / 16.0),
-      new Vector3(8 / 16.0, 0 / 16.0, 0.8 / 16.0),
+      new Vector3(8 / 16.0, 16 / 16.0, 0 / 16.0),
+      new Vector3(8 / 16.0, 16 / 16.0, 16 / 16.0),
+      new Vector3(8 / 16.0, 0 / 16.0, 0 / 16.0),
       new Vector4(16 / 16.0, 0 / 16.0, 16 / 16.0, 0 / 16.0)
     ),
     new Quad(
