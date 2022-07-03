5301b325a89f023b5e535f15a58f67b8c46adf55
Maik Marschner <an@maik.tk>
2022-07-03 20:11:41 +0200

    Fix sub-models not overriding the parent model's elements.

--- chunky/src/java/se/llbit/chunky/block/jsonmodels/ResourcepackBlockProvider.java
+++ chunky/src/java/se/llbit/chunky/block/jsonmodels/ResourcepackBlockProvider.java
@@ -454,7 +454,7 @@ public class ResourcepackBlockProvider implements BlockProvider {
 
       JsonModel block = new JsonModel(blockName, Texture.air);
       JsonObject blockDefinition = this.getModel(resourcePacks, model);
-      block.applyDefinition(blockDefinition, name -> this.getTexture(resourcePacks, name));
+      block.applyDefinition(blockDefinition, name -> this.getTexture(resourcePacks, name), false);
       String parentName = blockDefinition.get("parent").stringValue("block/block");
       if (parentName.equals("block/cube_all") || parentName.equals("minecraft:block/cube_all")) {
         // System.out.println("optimized block/cube_all");
@@ -472,8 +472,9 @@ public class ResourcepackBlockProvider implements BlockProvider {
       try {
         while (!blockDefinition.get("parent").isUnknown()) {
           parentName = blockDefinition.get("parent").stringValue("block/block");
+          boolean ignoreParentElements = blockDefinition.get("elements").isArray();
           blockDefinition = this.getModel(resourcePacks, parentName);
-          block.applyDefinition(blockDefinition, name -> this.getTexture(resourcePacks, name));
+          block.applyDefinition(blockDefinition, name -> this.getTexture(resourcePacks, name), ignoreParentElements);
           block.texture = block.textures.get("particle");
           if (parentName.equals("block/cube_all") || parentName
               .equals("minecraft:block/cube_all")) {
@@ -796,7 +797,7 @@ public class ResourcepackBlockProvider implements BlockProvider {
       return qb;
     }
 
-    public void applyDefinition(JsonObject modelDefinition, Function<String, Texture> getTexture) {
+    public void applyDefinition(JsonObject modelDefinition, Function<String, Texture> getTexture, boolean ignoreElements) {
       if (modelDefinition.get("textures").isObject()) {
         for (JsonMember texture : modelDefinition.get("textures").object().members) {
           if (texture.getValue().stringValue("").charAt(0) == '#') {
@@ -814,7 +815,7 @@ public class ResourcepackBlockProvider implements BlockProvider {
         }
       }
 
-      if (modelDefinition.get("elements").isArray()) {
+      if (!ignoreElements && modelDefinition.get("elements").isArray()) {
         for (JsonValue e : modelDefinition.get("elements").array()) {
           JsonModelElement element = new JsonModelElement(this, e.asObject());
           // prepend to make overlays work (e.g. for the grass block)
