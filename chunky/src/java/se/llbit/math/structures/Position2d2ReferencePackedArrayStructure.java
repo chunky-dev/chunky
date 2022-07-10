116e3164d130fd997c7b46424d71285545b7bd1f
Alex <alexhliu02@gmail.com>
2022-07-10 11:34:35 -0700

    Fix same issue in Position2d2ReferencePackedArrayStructure.

--- chunky/src/java/se/llbit/math/structures/Position2d2ReferencePackedArrayStructure.java
+++ chunky/src/java/se/llbit/math/structures/Position2d2ReferencePackedArrayStructure.java
@@ -7,8 +7,8 @@ public class Position2d2ReferencePackedArrayStructure<T> implements Position2Ref
 
   protected final Long2ReferenceMap<T[]> map = new Long2ReferenceOpenHashMap<>();
 
-  private long packedSectionPos(int x, int z) {
-    return (z & 0xFFFFFFFFL) | (x & 0xFFFFFFFFL) << 32;
+  private long packedSectionPos(int cx, int cz) {
+    return (cz & 0xFFFFFFFFL) | (cx & 0xFFFFFFFFL) << 32;
   }
 
   private int packedIndex(int x, int y, int z) {
@@ -19,12 +19,18 @@ public class Position2d2ReferencePackedArrayStructure<T> implements Position2Ref
 
   @Override
   public void set(int x, int y, int z, T data) {
-    this.map.computeIfAbsent(packedSectionPos(x, z), sectionPos -> (T[]) new Object[16 * 16])[packedIndex(x, y, z)] = data;
+    int cx = x >> 4;
+    int cz = z >> 4;
+    long sp = packedSectionPos(cx, cz);
+    this.map.computeIfAbsent(sp, sectionPos -> (T[]) new Object[16 * 16])[packedIndex(x, y, z)] = data;
   }
 
   @Override
   public T get(int x, int y, int z) {
-    T[] tArray = this.map.get(packedSectionPos(x, z));
+    int cx = x >> 4;
+    int cz = z >> 4;
+    long sp = packedSectionPos(cx, cz);
+    T[] tArray = this.map.get(sp);
     if(tArray == null) {
       return null;
     }
