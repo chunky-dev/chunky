bb67471d97cdc7dff5322869e282da54403c4cd7
Maik Marschner <an@maik.tk>
2022-10-04 23:56:48 +0200

    Do not optimize cube_all blocks if the texture is not opaque.

--- chunky/src/java/se/llbit/chunky/resources/Texture.java
+++ chunky/src/java/se/llbit/chunky/resources/Texture.java
@@ -1931,6 +1931,15 @@ public class Texture {
     return image;
   }
 
+  public boolean hasOpacity() {
+    for (int i = 0; i < image.data.length; i++) {
+      if (image.data[i] >>> 24 < 255) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   public static void setUseAverageColor(boolean useAverageColor) {
     Texture.useAverageColor = useAverageColor;
   }
