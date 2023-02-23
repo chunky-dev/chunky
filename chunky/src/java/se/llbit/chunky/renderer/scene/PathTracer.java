b4b5df79e0e290fe44b9e44396ade719bcd9ef85
Peregrine05 <92183530+Peregrine05@users.noreply.github.com>
2023-02-23 18:02:52 +0100

    Use correct implementation
    [Discord discussion](https://discord.com/channels/541221265512464394/545374333883777037/1078356004154114069)

--- chunky/src/java/se/llbit/chunky/renderer/scene/PathTracer.java
+++ chunky/src/java/se/llbit/chunky/renderer/scene/PathTracer.java
@@ -170,9 +170,9 @@ public class PathTracer implements RayTracer {
             if (scene.emittersEnabled && (!scene.isPreventNormalEmitterWithSampling() || scene.getEmitterSamplingStrategy() == EmitterSamplingStrategy.NONE || ray.depth == 0) && currentMat.emittance > Ray.EPSILON) {
 
               if (firstReflection) {
-                ray.apparentBrightness.x = ray.color.x * currentMat.apparentBrightness * scene.apparentEmitterBrightness;
-                ray.apparentBrightness.y = ray.color.y * currentMat.apparentBrightness * scene.apparentEmitterBrightness;
-                ray.apparentBrightness.z = ray.color.z * currentMat.apparentBrightness * scene.apparentEmitterBrightness;
+                ray.apparentBrightness.x = ray.color.x * ray.color.x * currentMat.apparentBrightness * scene.apparentEmitterBrightness;
+                ray.apparentBrightness.y = ray.color.y * ray.color.y * currentMat.apparentBrightness * scene.apparentEmitterBrightness;
+                ray.apparentBrightness.z = ray.color.z * ray.color.z * currentMat.apparentBrightness * scene.apparentEmitterBrightness;
 
               } else {
                 ray.emittance.x = ray.color.x * ray.color.x *
