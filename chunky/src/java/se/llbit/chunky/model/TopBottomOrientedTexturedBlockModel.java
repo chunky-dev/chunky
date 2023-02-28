6230a4093d57da8335374133af6bf3cf2179c98c
Maximilian Stiede <maximilian.stiede@student.hpi.de>
2023-02-28 15:35:15 +0100

    add support for custom quads in TopBottomOrientedTexturedBlockModel

--- chunky/src/java/se/llbit/chunky/model/TopBottomOrientedTexturedBlockModel.java
+++ chunky/src/java/se/llbit/chunky/model/TopBottomOrientedTexturedBlockModel.java
@@ -2,61 +2,48 @@ package se.llbit.chunky.model;
 
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Quad;
-import se.llbit.math.Vector3;
-import se.llbit.math.Vector4;
 
+/**
+ * A quad-based BlockModel which can be rotated horizontally (around y-axis) using a given facing.
+ * Defaults to a full block, but can be customized by providing own quads.
+ */
 public class TopBottomOrientedTexturedBlockModel extends QuadModel {
-  private static final Quad[] side = {
-      // north
-      new Quad(new Vector3(1, 0, 0), new Vector3(0, 0, 0), new Vector3(1, 1, 0),
-          new Vector4(0, 1, 0, 1)),
 
-      // south
-      new Quad(new Vector3(0, 0, 1), new Vector3(1, 0, 1), new Vector3(0, 1, 1),
-          new Vector4(0, 1, 0, 1)),
+  protected final Quad[] quads;
+  protected final Texture[] textures;
 
-      // west
-      new Quad(new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(0, 1, 0),
-          new Vector4(0, 1, 0, 1)),
+  /**
+   * @param facing accepted values: "north", "south", "west", "east"
+   * @param quads structured like: [north, south, west, east, top, bottom]
+   * @param textures structured like: [north, south, west, east, top, bottom]
+   */
+  public TopBottomOrientedTexturedBlockModel(
+    String facing,
+    Quad[] quads,
+    Texture[] textures
+  ) {
+    this.quads = rotateToFacing(facing, quads);
+    this.textures = textures;
+  }
 
-      // east
-      new Quad(new Vector3(1, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 1, 1),
-          new Vector4(0, 1, 0, 1)),
+  public TopBottomOrientedTexturedBlockModel(String facing,
+    Texture north, Texture east, Texture south, Texture west, Texture top, Texture bottom) {
+    this(facing, FULL_BLOCK_QUADS, new Texture[]{north, south, west, east, top, bottom});
+  }
 
-      // top
-      new Quad(new Vector3(1, 1, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 1),
-          new Vector4(1, 0, 1, 0)),
-
-      // bottom
-      new Quad(new Vector3(0, 0, 0), new Vector3(1, 0, 0), new Vector3(0, 0, 1),
-          new Vector4(0, 1, 0, 1)),
-  };
-
-  private final Quad[] quads;
-  private final Texture[] textures;
-
-  public TopBottomOrientedTexturedBlockModel(String facing, Texture north, Texture east, Texture south,
-                                             Texture west, Texture top, Texture bottom) {
+  private Quad[] rotateToFacing(String facing, Quad[] quads) {
     switch (facing) {
       case "north":
-        quads = side;
-        break;
+        return quads;
       case "south":
-        quads = Model.rotateY(Model.rotateY(side));
-        break;
+        return Model.rotateY(Model.rotateY(quads));
       case "east":
-        quads = Model.rotateY(side, -Math.toRadians(90));
-        break;
+        return Model.rotateY(quads, -Math.toRadians(90));
       case "west":
-        quads = Model.rotateNegY(side);
-        break;
+        return Model.rotateNegY(quads);
       default:
-        throw new IllegalArgumentException(("Invalid facing: " + facing));
+        throw new IllegalArgumentException("Invalid facing: " + facing);
     }
-
-    textures = new Texture[] {
-        north, south, west, east, top, bottom
-    };
   }
 
   @Override
