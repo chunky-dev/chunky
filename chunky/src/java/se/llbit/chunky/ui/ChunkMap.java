37035ffd77de2d23e55648a8b2c5b92d095e1295
Alex <42661490+ThatRedox@users.noreply.github.com>
2023-03-12 13:20:21 -0700

    Add an API to get the canvas / map coordinate in the context menu callback. (#1557)
    * Add the canvas / map coordinates as the userdata in the context menu.
    
    * Switch to context menu properties instead of user data.
    
    * Calculate chunk position for the map view. Clarify javadocs.
    
    * Calculate chunk position for the map view. Clarify javadocs.
    
    * Apply plugin api comment suggestion.
    
    Co-authored-by: Maik Marschner <m.marschner@wertarbyte.com>
    
    ---------
    
    Co-authored-by: Maik Marschner <m.marschner@wertarbyte.com>

--- chunky/src/java/se/llbit/chunky/ui/ChunkMap.java
+++ chunky/src/java/se/llbit/chunky/ui/ChunkMap.java
@@ -511,6 +511,16 @@ public class ChunkMap implements ChunkUpdateListener, ChunkViewListener, CameraV
     if (event.getButton() == MouseButton.SECONDARY) {
       clickX = lastX;
       clickY = lastY;
+
+      ChunkView theView = mapView.getMapView();
+      Vector2 overlayPosition = new Vector2(event.getX(), event.getY());
+      Vector2 chunkPosition = new Vector2(
+        theView.x + (overlayPosition.x - getWidth() / 2f) / theView.scale,
+        theView.z + (overlayPosition.y - getHeight() / 2f) / theView.scale
+      );
+
+      contextMenu.getProperties().put("overlayPosition", overlayPosition); // plugin api
+      contextMenu.getProperties().put("chunkPosition", chunkPosition); // plugin api
       contextMenu.show(mapOverlay, event.getScreenX(), event.getScreenY());
     } else {
       if (contextMenu.isShowing()) {
