69080fd84fe0c5e689ee33e1e923a701712a9841
Alex <alexhliu02@gmail.com>
2023-03-28 20:55:02 -0700

    Use textured block model for simple oriented blocks.

--- chunky/src/java/se/llbit/chunky/model/TexturedBlockModel.java
+++ chunky/src/java/se/llbit/chunky/model/TexturedBlockModel.java
@@ -16,12 +16,13 @@
  */
 package se.llbit.chunky.model;
 
-import se.llbit.chunky.block.Air;
+import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
+import se.llbit.log.Log;
 import se.llbit.math.AABB;
-import se.llbit.math.QuickMath;
 import se.llbit.math.Ray;
-import se.llbit.math.Vector3;
+import se.llbit.util.annotation.NotNull;
+import se.llbit.util.annotation.Nullable;
 
 /**
  * A textured block.
@@ -29,68 +30,125 @@ import se.llbit.math.Vector3;
  * @author Jesper Öqvist <jesper@llbit.se>
  */
 public class TexturedBlockModel extends AABBModel {
-  private static final AABB[] boxes = { new AABB(0, 1, 0, 1, 0, 1) };
+  public enum Orientation {
+    NORTH, SOUTH, EAST, WEST,
+    SIDE_NORTH, SIDE_SOUTH, SIDE_EAST, SIDE_WEST,
 
-  private final Texture[][] textures;
+    ;
+    public static Orientation fromFacing(String facing, boolean side) {
+      switch (facing) {
+        case "north":
+          return side ? SIDE_NORTH : NORTH;
+        case "south":
+          return side ? SIDE_SOUTH : SOUTH;
+        case "east":
+          return side ? SIDE_EAST : EAST;
+        case "west":
+          return side ? SIDE_WEST : WEST;
+        default:
+          throw new IllegalArgumentException("Invalid facing: " + facing);
+      }
+    }
+  }
+
+  protected static final AABB box = new AABB(0, 1, 0, 1, 0, 1);
+  protected final Texture[] textures;
+  protected final UVMapping[] mappings;
+  protected final Tint[] tints;
 
   public TexturedBlockModel(Texture north, Texture east, Texture south, Texture west, Texture top, Texture bottom) {
-    this.textures = new Texture[][] {{
-      north, east, south, west, top, bottom
-    }};
+    this(new Texture[] { north, east, south, west, top, bottom }, null, null);
+  }
+
+  public TexturedBlockModel(Orientation orientation, Texture north, Texture east, Texture south, Texture west,
+                            Texture top, Texture bottom, @Nullable Tint[] tints) {
+    this(
+      mapTextures(orientation, north, east, south, west, top, bottom),
+      mapUV(orientation),
+      tints
+    );
+  }
+
+  public TexturedBlockModel(@NotNull Texture[] textures, @Nullable UVMapping[] mappings, @Nullable Tint[] tints) {
+    this.textures = textures;
+    this.textures2 = new Texture[][] { textures };
+
+    this.mappings = mappings;
+    this.mappings2 = mappings == null ? null : new UVMapping[][] { mappings };
+
+    this.tints = tints;
+    this.tints2 = tints == null ? null : new Tint[][] { tints };
+  }
+
+  private static Texture[] mapTextures(Orientation orientation, Texture north, Texture east, Texture south,
+                                       Texture west, Texture top, Texture bottom) {
+    switch (orientation) {
+      default:
+        Log.warn("Unknown orientation: " + orientation);
+      case NORTH:
+      case SIDE_NORTH:
+        return new Texture[] { north, east, south, west, top, bottom };
+      case SOUTH:
+      case SIDE_SOUTH:
+        return new Texture[] { south, west, north, east, top, bottom };
+      case EAST:
+      case SIDE_EAST:
+        return new Texture[] { west, north, east, south, top, bottom };
+      case WEST:
+      case SIDE_WEST:
+        return new Texture[] { east, south, west, north, top, bottom };
+    }
+  }
+
+  private static UVMapping[] mapUV(Orientation orientation) {
+    switch (orientation) {
+      default:
+        Log.warn("Unknown orientation: " + orientation);
+      case NORTH:
+        return null;
+      case SOUTH:
+        return new UVMapping[] {null, null, null, null, UVMapping.ROTATE_180, UVMapping.ROTATE_180};
+      case EAST:
+        return new UVMapping[] {null, null, null, null, UVMapping.ROTATE_90, UVMapping.ROTATE_90};
+      case WEST:
+        return new UVMapping[] {null, null, null, null, UVMapping.ROTATE_270, UVMapping.ROTATE_270};
+      case SIDE_EAST:
+      case SIDE_SOUTH:
+      case SIDE_NORTH:
+      case SIDE_WEST:
+        return null;
+    }
   }
 
   @Override
-  public AABB[] getBoxes() {
+  public boolean intersect(Ray ray, Scene scene) {
+    // TODO: Optimize
+    return super.intersect(ray, scene);
+  }
+
+  // This stuff is just to adhere to the interface.
+  private static final AABB[] boxes = { box };
+  private final Texture[][] textures2;
+  private final UVMapping[][] mappings2;
+  private final Tint[][] tints2;
+
+  @Override
+  public final AABB[] getBoxes() {
     return boxes;
   }
 
   @Override
-  public Texture[][] getTextures() {
-    return textures;
+  public final Texture[][] getTextures() {
+    return textures2;
   }
 
-  /**
-   * Find the color of the object at the intersection point.
-   *
-   * @param ray ray to test
-   */
-  public static void getIntersectionColor(Ray ray) {
-    if (ray.getCurrentMaterial() == Air.INSTANCE) {
-      ray.color.x = 1;
-      ray.color.y = 1;
-      ray.color.z = 1;
-      ray.color.w = 0;
-      return;
-    }
-    getTextureCoordinates(ray);
-    ray.getCurrentMaterial().getColor(ray);
+  @Override
+  public final UVMapping[][] getUVMapping() {
+    return mappings2;
   }
 
-  /**
-   * Calculate the UV coordinates for the ray on the intersected block.
-   *
-   * @param ray ray to test
-   */
-  private static void getTextureCoordinates(Ray ray) {
-    int bx = (int) QuickMath.floor(ray.o.x);
-    int by = (int) QuickMath.floor(ray.o.y);
-    int bz = (int) QuickMath.floor(ray.o.z);
-    Vector3 n = ray.getNormal();
-    if (n.y != 0) {
-      ray.u = ray.o.x - bx;
-      ray.v = ray.o.z - bz;
-    } else if (n.x != 0) {
-      ray.u = ray.o.z - bz;
-      ray.v = ray.o.y - by;
-    } else {
-      ray.u = ray.o.x - bx;
-      ray.v = ray.o.y - by;
-    }
-    if (n.x > 0 || n.z < 0) {
-      ray.u = 1 - ray.u;
-    }
-    if (n.y > 0) {
-      ray.v = 1 - ray.v;
-    }
+  @Override
+  public final Tint[][] getTints() {
+    return tints2;
   }
 }
