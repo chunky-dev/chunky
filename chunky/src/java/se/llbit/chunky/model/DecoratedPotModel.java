216f1e486c69574444a34f157f738aa487914e8b
JustinTimeCuber <justintimecuber@gmail.com>
2023-07-08 18:55:49 -0500

    fix decorated pot model

--- chunky/src/java/se/llbit/chunky/model/DecoratedPotModel.java
+++ chunky/src/java/se/llbit/chunky/model/DecoratedPotModel.java
@@ -1,6 +1,5 @@
 package se.llbit.chunky.model;
 
-import se.llbit.chunky.entity.SporeBlossom;
 import se.llbit.chunky.resources.Texture;
 import se.llbit.log.Log;
 import se.llbit.math.Quad;
@@ -20,7 +19,8 @@ import java.util.LinkedList;
 
 public class DecoratedPotModel extends TopBottomOrientedTexturedBlockModel {
 
-  private static final Vector4 BASE_UV_MAP = new Vector4(0 / 32., 14 / 32., 8 / 32., 22 / 32.);
+  private static final Vector4 TOP_UV_MAP = new Vector4(14 / 32., 28 / 32., 19 / 32., 5 / 32.);
+  private static final Vector4 BOTTOM_UV_MAP = new Vector4(6.5 / 16, 6.5 / 16, 5.5 / 16, 5.5 / 16);
   private static final Vector4 SIDE_UV_MAP = new Vector4(1 / 16., 15 / 16., 0., 1.);
 
   private static final Quad NORTH_SIDE = new Quad(
@@ -47,12 +47,12 @@ public class DecoratedPotModel extends TopBottomOrientedTexturedBlockModel {
     new Vector3(1 / 16., 1, 15 / 16.),
     new Vector3(15 / 16., 1, 15 / 16.),
     new Vector3(1 / 16., 1, 1 / 16.),
-    BASE_UV_MAP);
+    TOP_UV_MAP);
   private static final Quad BOTTOM_SIDE = new Quad(
     new Vector3(1 / 16., 0, 1 / 16.),
     new Vector3(15 / 16., 0, 1 / 16.),
     new Vector3(1 / 16., 0, 15 / 16.),
-    BASE_UV_MAP);
+    BOTTOM_UV_MAP);
 
   private static final Quad[] DEFAULT_QUADS = {
     NORTH_SIDE, SOUTH_SIDE,
@@ -63,31 +63,59 @@ public class DecoratedPotModel extends TopBottomOrientedTexturedBlockModel {
   public static class DecoratedPotSpoutEntity extends Entity {
 
     private static final Quad[] QUADS = {
+      // top section
+      new Quad( // north
+        new Vector3(12 / 16., 17 / 16., 4 / 16.),
+        new Vector3(4 / 16., 17 / 16., 4 / 16.),
+        new Vector3(12 / 16., 20 / 16., 4 / 16.),
+        new Vector4(24 / 32., 32 / 32., 21 / 32., 24 / 32.)),
+      new Quad( // south
+        new Vector3(4 / 16., 17 / 16., 12 / 16.),
+        new Vector3(12 / 16., 17 / 16., 12 / 16.),
+        new Vector3(4 / 16., 20 / 16., 12 / 16.),
+        new Vector4(8 / 32., 16 / 32., 21 / 32., 24 / 32.)),
+      new Quad( // west
+        new Vector3(4 / 16., 17 / 16., 4 / 16.),
+        new Vector3(4 / 16., 17 / 16., 12 / 16.),
+        new Vector3(4 / 16., 20 / 16., 4 / 16.),
+        new Vector4(0 / 32., 8 / 32., 21 / 32., 24 / 32.)),
+      new Quad( // east
+        new Vector3(12 / 16., 17 / 16., 12 / 16.),
+        new Vector3(12 / 16., 17 / 16., 4 / 16.),
+        new Vector3(12 / 16., 20 / 16., 12 / 16.),
+        new Vector4(16 / 32., 24 / 32., 21 / 32., 24 / 32.)),
+      new Quad( // top
+        new Vector3(4 / 16., 20 / 16., 12 / 16.),
+        new Vector3(12 / 16., 20 / 16., 12 / 16.),
+        new Vector3(4 / 16., 20 / 16., 4 / 16.),
+        new Vector4(8 / 32., 16 / 32., 24 / 32., 32 / 32.)),
+      new Quad( // bottom
+        new Vector3(4 / 16., 17 / 16., 4 / 16.),
+        new Vector3(12 / 16., 17 / 16., 4 / 16.),
+        new Vector3(4 / 16., 17 / 16., 12 / 16.),
+        new Vector4(16 / 32., 24 / 32., 32 / 32., 24 / 32.)),
+
+      // throat
       new Quad( // north
         new Vector3(11 / 16., 1, 5 / 16.),
         new Vector3(5 / 16., 1, 5 / 16.),
-        new Vector3(11 / 16., 20 / 16., 5 / 16.),
-        new Vector4(18 / 32., 24 / 32., 22 / 32., 26 / 32.)),
+        new Vector3(11 / 16., 17 / 16., 5 / 16.),
+        new Vector4(18 / 32., 24 / 32., 20 / 32., 21 / 32.)),
       new Quad( // south
         new Vector3(5 / 16., 1, 11 / 16.),
         new Vector3(11 / 16., 1, 11 / 16.),
-        new Vector3(5 / 16., 20 / 16., 11 / 16.),
-        new Vector4(6 / 32., 12 / 32., 22 / 32., 26 / 32.)),
+        new Vector3(5 / 16., 17 / 16., 11 / 16.),
+        new Vector4(6 / 32., 12 / 32., 20 / 32., 21 / 32.)),
       new Quad( // west
         new Vector3(5 / 16., 1, 5 / 16.),
         new Vector3(5 / 16., 1, 11 / 16.),
-        new Vector3(5 / 16., 20 / 16., 5 / 16.),
-        new Vector4(0 / 32., 6 / 32., 22 / 32., 26 / 32.)),
+        new Vector3(5 / 16., 17 / 16., 5 / 16.),
+        new Vector4(0 / 32., 6 / 32., 20 / 32., 21 / 32.)),
       new Quad( // east
         new Vector3(11 / 16., 1, 11 / 16.),
         new Vector3(11 / 16., 1, 5 / 16.),
-        new Vector3(11 / 16., 20 / 16., 11 / 16.),
-        new Vector4(12 / 32., 18 / 32., 22 / 32., 26 / 32.)),
-      new Quad( // top
-        new Vector3(5 / 16., 20 / 16., 11 / 16.),
-        new Vector3(11 / 16., 20 / 16., 11 / 16.),
-        new Vector3(5 / 16., 20 / 16., 5 / 16.),
-        new Vector4(6 / 32., 12 / 32., 26 / 32., 32 / 32.)),
+        new Vector3(11 / 16., 17 / 16., 11 / 16.),
+        new Vector4(12 / 32., 18 / 32., 20 / 32., 21 / 32.)),
     };
 
     private final Material material = new TextureMaterial(Texture.decoratedPotBase);
@@ -137,7 +165,7 @@ public class DecoratedPotModel extends TopBottomOrientedTexturedBlockModel {
       // sherds[2] right crafting slot -> east
       getTextureForsherd(sherds[2]),
       Texture.decoratedPotBase, // top
-      Texture.decoratedPotBase  // bottom
+      Texture.decoratedPotSide  // bottom
     });
   }
 
