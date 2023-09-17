d9787d9b0cef9c187aeb0479f56dac15c1852de4
Alex <redox@thatredox.dev>
2023-09-17 11:27:09 -0700

    Add Interner interface and WeakInterner implementation.

---
+++ chunky/src/java/se/llbit/util/interner/Interner.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023 Chunky contributors
+ *
+ * This file is part of Chunky.
+ *
+ * Chunky is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Chunky is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Chunky.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.llbit.util.interner;
+
+/**
+ * This deduplicates objects similar to `String.intern()`, but for arbitrary objects.
+ */
+public interface Interner<T> {
+  /**
+   * Intern the given object, returning an existing object if one exists.
+   */
+  T intern(T sample);
+}
---
+++ chunky/src/test/se/llbit/util/WeakInternerTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2023 Chunky contributors
+ *
+ * This file is part of Chunky.
+ *
+ * Chunky is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Chunky is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Chunky.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.llbit.util;
+
+import org.junit.Assume;
+import org.junit.Ignore;
+import org.junit.Test;
+import se.llbit.util.interner.Interner;
+import se.llbit.util.interner.WeakInterner;
+
+import java.lang.ref.WeakReference;
+import java.util.Objects;
+import java.util.Random;
+
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+public class WeakInternerTest {
+  private static class TestInternable {
+    public final long value;
+
+    public TestInternable(long value) {
+      this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof TestInternable)) return false;
+      TestInternable that = (TestInternable) o;
+      return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(value);
+    }
+  }
+
+  @Test
+  public void testSmoke() {
+    Random rand = new Random(0);
+    Interner<Object> interner = new WeakInterner<>();
+
+    TestInternable test11 = new TestInternable(rand.nextLong());
+    TestInternable test12 = new TestInternable(rand.nextLong());
+    TestInternable test13 = new TestInternable(rand.nextLong());
+    TestInternable test21 = new TestInternable(test11.value);
+    TestInternable test22 = new TestInternable(test12.value);
+    TestInternable test23 = new TestInternable(test13.value);
+
+    // Test that we get the same object back when interning the object.
+    assertSame(interner.intern(test11), test11);
+    assertSame(interner.intern(test12), test12);
+    assertSame(interner.intern(test13), test13);
+
+    // Test that we get back the interned object when interning the copy
+    assertSame(interner.intern(test21), test11);
+    assertSame(interner.intern(test22), test12);
+    assertSame(interner.intern(test23), test13);
+  }
+
+  @Test
+  public void testWeakness() {
+    Random rand = new Random(0);
+    Interner<Object> interner = new WeakInterner<>();
+
+    TestInternable test11 = new TestInternable(rand.nextLong());
+    TestInternable test12 = new TestInternable(rand.nextLong());
+    TestInternable test13 = new TestInternable(rand.nextLong());
+    TestInternable test21 = new TestInternable(test11.value);
+    TestInternable test22 = new TestInternable(test12.value);
+    TestInternable test23 = new TestInternable(test13.value);
+
+    // Test that we get the same object back when interning the object.
+    assertSame(interner.intern(test11), test11);
+    assertSame(interner.intern(test12), test12);
+    assertSame(interner.intern(test13), test13);
+
+    // Test that we get back the interned object when interning the copy
+    assertSame(interner.intern(test21), test11);
+    assertSame(interner.intern(test22), test12);
+    assertSame(interner.intern(test23), test13);
+
+    // Get references to the interned objects
+    WeakReference<TestInternable> weakTest1 = new WeakReference<>(test11);
+    WeakReference<TestInternable> weakTest2 = new WeakReference<>(test12);
+    WeakReference<TestInternable> weakTest3 = new WeakReference<>(test13);
+
+    // Clear their references
+    test11 = null;
+    test12 = null;
+    test13 = null;
+
+    // Wait until they are garbage collected
+    for (int i = 0; i < 1000; i++) {
+      System.gc();
+      if (weakTest1.get() == null && weakTest2.get() == null && weakTest3.get() == null) {
+        break;
+      }
+    }
+    Assume.assumeTrue(weakTest1.get() == null);
+    Assume.assumeTrue(weakTest2.get() == null);
+    Assume.assumeTrue(weakTest3.get() == null);
+
+    // Test that we get the copy back when interning the copy
+    assertSame(interner.intern(test21), test21);
+    assertSame(interner.intern(test22), test22);
+    assertSame(interner.intern(test23), test23);
+  }
+}
