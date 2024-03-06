2e3e4ab9bdb6936ede829da8e2f15c8e121840c7
Tom Martin <tom.martin1239@gmail.com>
2024-03-06 11:38:42 +0000

    Add additional documentation

--- chunky/src/java/se/llbit/chunky/plugin/manifest/PluginDependency.java
+++ chunky/src/java/se/llbit/chunky/plugin/manifest/PluginDependency.java
@@ -2,14 +2,17 @@ package se.llbit.chunky.plugin.manifest;
 
 import org.apache.maven.artifact.versioning.VersionRange;
 
+/**
+ * An unresolved plugin dependency within a plugin manifest.
+ */
 public class PluginDependency {
   public final String name;
   public final VersionRange version;
 
-    public PluginDependency(String name, VersionRange version) {
-        this.name = name;
-        this.version = version;
-    }
+  public PluginDependency(String name, VersionRange version) {
+    this.name = name;
+    this.version = version;
+  }
 
   @Override
   public String toString() {
