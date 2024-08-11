9ae58e8d0b4ed1a8c458a16cf2e3caf7c8290656
Maik Marschner <leMaik@leMaik.de>
2024-08-11 14:01:08 +0200

    Fix azalea leaves and cherry leaves ignoring material properties. (#1746)

--- chunky/src/java/se/llbit/chunky/block/UntintedLeaves.java
+++ chunky/src/java/se/llbit/chunky/block/UntintedLeaves.java
@@ -2,11 +2,8 @@ package se.llbit.chunky.block;
 
 import se.llbit.chunky.resources.Texture;
 
-public class UntintedLeaves extends Block {
-
+public class UntintedLeaves extends MinecraftBlockTranslucent {
   public UntintedLeaves(String name, Texture texture) {
     super(name, texture);
-    solid = false;
-    opaque = false;
   }
 }
