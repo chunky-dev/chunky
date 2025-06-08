d2d658f27ca39e380e8f548ea3a7d0cabfd1406e
Maik Marschner <leMaik@leMaik.de>
2025-06-08 20:50:03 +0200

    Add new repeater model.

--- chunky/src/java/se/llbit/chunky/block/minecraft/Repeater.java
+++ chunky/src/java/se/llbit/chunky/block/minecraft/Repeater.java
@@ -20,38 +20,29 @@ package se.llbit.chunky.block.minecraft;
 
 import se.llbit.chunky.block.AbstractModelBlock;
 import se.llbit.chunky.model.minecraft.RedstoneRepeaterModel;
+import se.llbit.chunky.model.minecraft.RedstoneRepeaterModel1212;
 import se.llbit.chunky.resources.Texture;
 
-// TODO: render locked repeaters.
 public class Repeater extends AbstractModelBlock {
-  private final int facing;
+  private final String facing;
   private final String description;
 
-  public Repeater(int delay, String facingString, boolean powered, boolean locked) {
+  public Repeater(int delay, String facing, boolean powered, boolean locked) {
     super("repeater", Texture.redstoneRepeaterOn);
-    this.description = String.format("delay=%d, facing=%s, powered=%s, locked=%s",
-        delay, facingString, powered, locked);
-    switch (facingString) {
-      default:
-      case "north":
-        facing = 2;
-        break;
-      case "south":
-        facing = 0;
-        break;
-      case "west":
-        facing = 1;
-        break;
-      case "east":
-        facing = 3;
-        break;
-    }
-    this.model = new RedstoneRepeaterModel(3 & (delay - 1), facing, powered ? 1 : 0,
-        locked ? 1 : 0);
+    description = String.format("delay=%d, facing=%s, powered=%s, locked=%s", delay, facing, powered, locked);
+    this.facing = facing;
+    model = System.getProperty("chunky.blockModels.redstoneTorch", "1.21.2").equals("pre-1.21.2")
+      ? new RedstoneRepeaterModel(facing, delay, powered, locked)
+      : new RedstoneRepeaterModel1212(facing, delay, powered, locked);
   }
 
   public int getFacing() {
-    return facing;
+    return switch (facing) {
+      case "south" -> 0;
+      case "west" -> 1;
+      case "east" -> 3;
+      default -> 2;
+    };
   }
 
   @Override
