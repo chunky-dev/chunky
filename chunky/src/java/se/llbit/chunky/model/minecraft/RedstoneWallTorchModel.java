e86e485c5c65e157d1bdb021b364cb3aaa1a252d
Maik Marschner <leMaik@leMaik.de>
2025-06-08 19:37:31 +0200

    Refactor the new torch glow intersection to be reusable across models.

--- chunky/src/java/se/llbit/chunky/model/minecraft/RedstoneWallTorchModel.java
+++ chunky/src/java/se/llbit/chunky/model/minecraft/RedstoneWallTorchModel.java
@@ -2,8 +2,10 @@ package se.llbit.chunky.model.minecraft;
 
 import se.llbit.chunky.model.Model;
 import se.llbit.chunky.model.QuadModel;
+import se.llbit.chunky.renderer.scene.Scene;
 import se.llbit.chunky.resources.Texture;
 import se.llbit.math.Quad;
+import se.llbit.math.Ray;
 import se.llbit.math.Vector3;
 import se.llbit.math.Vector4;
 
@@ -48,74 +50,38 @@ public class RedstoneWallTorchModel extends QuadModel {
           new Vector3(-1 / 16.0, 13.5 / 16.0, 9 / 16.0),
           new Vector3(1 / 16.0, 3.5 / 16.0, 9 / 16.0),
           new Vector4(9 / 16.0, 7 / 16.0, 10 / 16.0, 0 / 16.0)
-        )
-      },
-      Math.toRadians(-22.5),
-      new Vector3(0, 3.5 / 16., 0)
-    ),
-    Model.rotateZ(
-      new Quad[]{
-        new Quad(
+        ),
+        new RedstoneTorchModel.GlowQuad(
           new Vector3(-1.5 / 16.0, 11 / 16.0, 9.5 / 16.0),
           new Vector3(1.5 / 16.0, 11 / 16.0, 9.5 / 16.0),
           new Vector3(-1.5 / 16.0, 11 / 16.0, 6.5 / 16.0),
           new Vector4(6 / 16.0, 7 / 16.0, 10 / 16.0, 11 / 16.0)
-        )
-      },
-      Math.toRadians(-22.5),
-      new Vector3(0, 3.5 / 16., 0)
-    ),
-    Model.rotateZ(
-      new Quad[]{
-        new Quad(
+        ),
+        new RedstoneTorchModel.GlowQuad(
           new Vector3(-1.5 / 16.0, 14 / 16.0, 6.5 / 16.0),
           new Vector3(1.5 / 16.0, 14 / 16.0, 6.5 / 16.0),
           new Vector3(-1.5 / 16.0, 14 / 16.0, 9.5 / 16.0),
           new Vector4(6 / 16.0, 7 / 16.0, 10 / 16.0, 11 / 16.0)
-        )
-      },
-      Math.toRadians(-22.5),
-      new Vector3(0, 3.5 / 16., 0)
-    ),
-    Model.rotateZ(
-      new Quad[]{
-        new Quad(
+        ),
+        new RedstoneTorchModel.GlowQuad(
           new Vector3(1.5 / 16.0, 14 / 16.0, 6.5 / 16.0),
           new Vector3(-1.5 / 16.0, 14 / 16.0, 6.5 / 16.0),
           new Vector3(1.5 / 16.0, 11 / 16.0, 6.5 / 16.0),
           new Vector4(7 / 16.0, 6 / 16.0, 11 / 16.0, 10 / 16.0)
-        )
-      },
-      Math.toRadians(-22.5),
-      new Vector3(0, 3.5 / 16., 0)
-    ),
-    Model.rotateZ(
-      new Quad[]{
-        new Quad(
+        ),
+        new RedstoneTorchModel.GlowQuad(
           new Vector3(1.5 / 16.0, 14 / 16.0, 9.5 / 16.0),
           new Vector3(1.5 / 16.0, 14 / 16.0, 6.5 / 16.0),
           new Vector3(1.5 / 16.0, 11 / 16.0, 9.5 / 16.0),
           new Vector4(7 / 16.0, 6 / 16.0, 11 / 16.0, 10 / 16.0)
-        )
-      },
-      Math.toRadians(-22.5),
-      new Vector3(0, 3.5 / 16., 0)
-    ),
-    Model.rotateZ(
-      new Quad[]{
-        new Quad(
+        ),
+        new RedstoneTorchModel.GlowQuad(
           new Vector3(-1.5 / 16.0, 14 / 16.0, 9.5 / 16.0),
           new Vector3(1.5 / 16.0, 14 / 16.0, 9.5 / 16.0),
           new Vector3(-1.5 / 16.0, 11 / 16.0, 9.5 / 16.0),
           new Vector4(7 / 16.0, 6 / 16.0, 11 / 16.0, 10 / 16.0)
-        )
-      },
-      Math.toRadians(-22.5),
-      new Vector3(0, 3.5 / 16., 0)
-    ),
-    Model.rotateZ(
-      new Quad[]{
-        new Quad(
+        ),
+        new RedstoneTorchModel.GlowQuad(
           new Vector3(-1.5 / 16.0, 14 / 16.0, 6.5 / 16.0),
           new Vector3(-1.5 / 16.0, 14 / 16.0, 9.5 / 16.0),
           new Vector3(-1.5 / 16.0, 11 / 16.0, 6.5 / 16.0),
@@ -155,5 +121,10 @@ public class RedstoneWallTorchModel extends QuadModel {
   @Override
   public Texture[] getTextures() {
     return textures;
+  }
+
+  @Override
+  public boolean intersect(Ray ray, Scene scene) {
+    return RedstoneTorchModel.intersectWithGlow(ray, scene, this);
   }
 }
