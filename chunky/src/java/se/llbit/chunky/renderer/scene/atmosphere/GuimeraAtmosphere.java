8eb0ebbe56ed418b5c7a88257f01dd5d24b3f6ea
Peregrine05 <92183530+peregrine05@users.noreply.github.com>
2025-08-27 21:49:28 -0400

    Remove mistakenly added files

--- chunky/src/java/se/llbit/chunky/renderer/scene/atmosphere/GuimeraAtmosphere.java
+++
@@ -1,5 +0,0 @@
-package se.llbit.chunky.renderer.scene.atmosphere;
-
-public class GuimeraAtmosphere {
-
-}
--- chunky/src/java/se/llbit/chunky/renderer/scene/atmosphere/aerosol/DesertDustAerosol.java
+++
@@ -1,4 +0,0 @@
-package se.llbit.chunky.renderer.scene.atmosphere.aerosol;
-
-public class DesertDustAerosol {
-}
