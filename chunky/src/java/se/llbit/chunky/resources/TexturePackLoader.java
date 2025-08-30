15a447a1d7cbfc6a5685e781364fdf02ac32df80
Maik Marschner <leMaik@leMaik.de>
2025-08-30 15:34:57 +0200

    Rename chain to iron_chain but keep compatibility with chain. (#1832)

--- chunky/src/java/se/llbit/chunky/resources/TexturePackLoader.java
+++ chunky/src/java/se/llbit/chunky/resources/TexturePackLoader.java
@@ -3484,7 +3484,6 @@ public class TexturePackLoader {
     addSimpleTexture("assets/minecraft/textures/block/polished_blackstone_bricks", Texture.polishedBlackstoneBricks);
     addSimpleTexture("assets/minecraft/textures/block/cracked_polished_blackstone_bricks", Texture.crackedPolishedBlackstoneBricks);
     addSimpleTexture("assets/minecraft/textures/block/quartz_bricks", Texture.quartzBricks);
-    addSimpleTexture("assets/minecraft/textures/block/chain", Texture.chain);
     addSimpleTexture("assets/minecraft/textures/block/jigsaw_lock", Texture.jigsawLock);
 
     // Minecraft 1.17
