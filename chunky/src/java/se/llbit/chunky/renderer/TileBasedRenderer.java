6c4a8e6aba4678c14124fd6806ada9e728ab8fda
Peregrine05 <92183530+peregrine05@users.noreply.github.com>
2025-09-02 23:28:16 -0400

    Slight code simplification

--- chunky/src/java/se/llbit/chunky/renderer/TileBasedRenderer.java
+++ chunky/src/java/se/llbit/chunky/renderer/TileBasedRenderer.java
@@ -77,10 +77,10 @@ public abstract class TileBasedRenderer implements Renderer {
             for (int j = tile.y0; j < tile.y1; j++) {
               pair.left(i).right(j);
               perPixel.accept(state, pair);
-              state.color.set(0, 0, 0, 0);
+              state.color.set(0);
               state.throughput.set(1);
               state.emittance.set(0);
-              state.sampleColor.set(0, 0, 0, 0);
+              state.sampleColor.set(0);
               state.attenuation.set(1);
             }
           }
