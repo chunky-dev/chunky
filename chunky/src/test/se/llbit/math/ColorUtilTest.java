8c19819a55b48b1c889cfb3e0803a7f9610e1611
Peregrine05 <92183530+peregrine05@users.noreply.github.com>
2025-09-13 03:06:23 -0400

    Update ColorUtilTest.java

--- chunky/src/test/se/llbit/math/ColorUtilTest.java
+++ chunky/src/test/se/llbit/math/ColorUtilTest.java
@@ -10,24 +10,24 @@ class ColorUtilTest {
   void fromHexString() {
     Vector3 color = new Vector3();
     ColorUtil.fromHexString("#c0ffee", color);
-    assertEquals(192 / 255., color.x, Ray.EPSILON);
-    assertEquals(255 / 255., color.y, Ray.EPSILON);
-    assertEquals(238 / 255., color.z, Ray.EPSILON);
+    assertEquals(192 / 255., color.x, Constants.EPSILON);
+    assertEquals(255 / 255., color.y, Constants.EPSILON);
+    assertEquals(238 / 255., color.z, Constants.EPSILON);
 
     ColorUtil.fromHexString("#123", color);
-    assertEquals(17 / 255., color.x, Ray.EPSILON);
-    assertEquals(34 / 255., color.y, Ray.EPSILON);
-    assertEquals(51 / 255., color.z, Ray.EPSILON);
+    assertEquals(17 / 255., color.x, Constants.EPSILON);
+    assertEquals(34 / 255., color.y, Constants.EPSILON);
+    assertEquals(51 / 255., color.z, Constants.EPSILON);
 
     ColorUtil.fromHexString("0ff1ce", color);
-    assertEquals(15 / 255., color.x, Ray.EPSILON);
-    assertEquals(241 / 255., color.y, Ray.EPSILON);
-    assertEquals(206 / 255., color.z, Ray.EPSILON);
+    assertEquals(15 / 255., color.x, Constants.EPSILON);
+    assertEquals(241 / 255., color.y, Constants.EPSILON);
+    assertEquals(206 / 255., color.z, Constants.EPSILON);
 
     ColorUtil.fromHexString("456", color);
-    assertEquals(68 / 255., color.x, Ray.EPSILON);
-    assertEquals(85 / 255., color.y, Ray.EPSILON);
-    assertEquals(102 / 255., color.z, Ray.EPSILON);
+    assertEquals(68 / 255., color.x, Constants.EPSILON);
+    assertEquals(85 / 255., color.y, Constants.EPSILON);
+    assertEquals(102 / 255., color.z, Constants.EPSILON);
 
     assertThrows(IllegalArgumentException.class, () -> {
       ColorUtil.fromHexString("1234", color);
