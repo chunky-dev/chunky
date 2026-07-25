559a6b792c106bc86d75464a08623843703a84c8
Maik Marschner <m.marschner@wertarbyte.com>
2026-07-25 20:46:11 +0200

    Use JUnit 5 for the launcher, too.

--- launcher/test/se/llbit/chunky/launcher/ChunkyLauncherCliParseTest.java
+++ launcher/test/se/llbit/chunky/launcher/ChunkyLauncherCliParseTest.java
@@ -20,10 +20,9 @@ package se.llbit.chunky.launcher;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.ParseException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
 
 public class ChunkyLauncherCliParseTest {
   @Test
